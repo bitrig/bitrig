5ac747c222bde7eb8606cd360c1c15924f6650a5
deraadt <deraadt@cvs.openbsd.org>
1998-10-28 17:56:03 +0000

    sync

--- sys/dev/isa/pnpdevs.h
+++ sys/dev/isa/pnpdevs.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	#	OpenBSD: pnpdevs,v 1.24 1998/10/28 17:55:10 deraadt Exp 
+ *	#	OpenBSD: pnpdevs,v 1.25 1998/10/28 17:56:00 deraadt Exp 
  */
 
 struct isapnp_knowndev isapnp_knowndevs[] = {
@@ -62,6 +62,8 @@ struct isapnp_knowndev isapnp_knowndevs[] = {
 	{ "USR0006", "pccom" },
 	{ "SUP1380", "com" },
 	{ "SUP1380", "pccom" },
+	{ "GVC5000", "com" },
+	{ "GVC5000", "pccom" },
 	{ "ADS7150", "sb" },
 	{ "ADS7180", "sb" },
 	{ "CTL0001", "sb" },
