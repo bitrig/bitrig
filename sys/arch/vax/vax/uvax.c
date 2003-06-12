2b096da307c629cadb87135e9a623b040565baf4
deraadt <deraadt@cvs.openbsd.org>
2003-06-12 01:07:32 +0000

    final bits of obvious UCB term 3 removal

--- sys/arch/vax/vax/uvax.c
+++ sys/arch/vax/vax/uvax.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvax.c,v 1.8 2003/06/12 01:07:27 deraadt Exp $ */
+/*	$OpenBSD: uvax.c,v 1.9 2003/06/12 01:07:31 deraadt Exp $ */
 /*	$NetBSD: uvax.c,v 1.4 1997/02/19 10:04:27 ragge Exp $ */
 /*
  * Copyright (c) 1996 Ludd, University of Lule}, Sweden.
