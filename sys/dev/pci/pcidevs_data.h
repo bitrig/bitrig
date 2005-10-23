7c07372aab9079299d2492b4990fb0230b7456aa
brad <brad@cvs.openbsd.org>
2005-10-23 20:10:17 +0000

    regen

--- sys/dev/pci/pcidevs_data.h
+++ sys/dev/pci/pcidevs_data.h
@@ -2,7 +2,7 @@
  * THIS FILE AUTOMATICALLY GENERATED.  DO NOT EDIT.
  *
  * generated from:
- *	OpenBSD: pcidevs,v 1.946 2005/10/22 22:59:05 brad Exp 
+ *	OpenBSD: pcidevs,v 1.947 2005/10/23 20:09:10 brad Exp 
  */
 /*	$NetBSD: pcidevs,v 1.30 1997/06/24 06:20:24 thorpej Exp $	*/
 
