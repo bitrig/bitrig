210651b8f4311aab5f0b5c3c2132a741bda968a4
Marco Peereboom <marco@conformal.com>
2012-03-02 17:38:53 -0600

    bring textinfo back

---
+++ gnu/usr.bin/texinfo/lib/getopt.h
@@ -0,0 +1,10 @@
+/* getopt.h -- wrapper for gnulib getopt_.h.
+   $Id: getopt.h,v 1.1.1.5 2006/07/17 16:03:41 espie Exp $
+   Copyright (C) 2004 Free Software Foundation, Inc.
+
+   Copying and distribution of this file, with or without modification,
+   are permitted in any medium without royalty provided the copyright
+   notice and this notice are preserved.
+*/
+
+#include "getopt_.h"
