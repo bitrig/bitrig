0174641bffe0d5ecd24da6b569b0c407e3063419
Artur Grabowski <art@blahonga.org>
2012-06-14 14:00:24 +0200

    Convert the vga_post mappings to km_alloc
    Diff 22 from the large batch of km_alloc diffs from oga@

--- sys/arch/i386/pci/vga_post.c
+++ sys/arch/i386/pci/vga_post.c
@@ -129,13 +129,15 @@ vga_post_init(int bus, int device, int function)
 	vaddr_t sys_image, sys_bios_data;
 	int err;
 
-	sys_bios_data = uvm_km_valloc(kernel_map, PAGE_SIZE);
+	sys_bios_data = (vaddr_t)km_alloc(PAGE_SIZE, &kv_any, &kp_none,
+	    &kd_nowait);;
 	if (sys_bios_data == 0)
 		return NULL;
 
-	sys_image = uvm_km_valloc(kernel_map, 1024 * 1024);
+	sys_image = (vaddr_t)km_alloc(1024 * 1024, &kv_any, &kp_none,
+	    &kd_nowait);
 	if (sys_image == 0) {
-		uvm_km_free(kernel_map, sys_bios_data, PAGE_SIZE);
+		km_free((void *)sys_bios_data, PAGE_SIZE, &kv_any, &kp_none);
 		return NULL;
 	}
 	sc = malloc(sizeof(*sc), M_DEVBUF, M_WAITOK|M_ZERO);
@@ -144,8 +146,9 @@ vga_post_init(int bus, int device, int function)
 	err = uvm_pglistalloc(BASE_MEMORY, 0, (paddr_t)-1, 0, 0,
 	    &sc->ram_backing, BASE_MEMORY/PAGE_SIZE, UVM_PLA_WAITOK);
 	if (err) {
-		uvm_km_free(kernel_map, sc->sys_image, 1024 * 1024);
 		free(sc, M_DEVBUF);
+		km_free((void *)sys_image, 1024 * 1024, &kv_any, &kp_dma);
+		km_free((void *)sys_bios_data, PAGE_SIZE, &kv_any, &kp_none);
 		return NULL;
 	}
 
@@ -156,7 +159,7 @@ vga_post_init(int bus, int device, int function)
 	pmap_update(pmap_kernel());
 	memcpy((void *)sc->bios_data, (void *)sys_bios_data, PAGE_SIZE);
 	pmap_kremove(sys_bios_data, PAGE_SIZE);
-	uvm_km_free(kernel_map, sys_bios_data, PAGE_SIZE);
+	km_free((void *)sys_bios_data, PAGE_SIZE, &kv_any, &kp_none);
 
 	iter = 0;
 	TAILQ_FOREACH(pg, &sc->ram_backing, pageq) {
@@ -213,9 +216,9 @@ vga_post_free(struct vga_post *sc)
 {
 	uvm_pglistfree(&sc->ram_backing);
 	pmap_kremove(sc->sys_image, 1024 * 1024);
-
-	uvm_km_free(kernel_map, sc->sys_image, 1024 * 1024);
 	pmap_update(pmap_kernel());
+
+	km_free((void *)sc->sys_image, 1024 * 1024, &kv_any, &kp_dma);
 	free(sc, M_DEVBUF);
 }
 
