ca289e8c2ec54e04afcf1d0608f4b2705e9c864f
Damien Miller <djm@cvs.openbsd.org>
2012-10-13 21:23:38 +0000

    import OpenSSL-1.0.1c

--- lib/libssl/src/crypto/bn/asm/x86_64-gf2m.pl
+++ lib/libssl/src/crypto/bn/asm/x86_64-gf2m.pl
@@ -31,8 +31,7 @@ $0 =~ m/(.*[\/\\])[^\/\\]+$/; $dir=$1;
 ( $xlate="${dir}../../perlasm/x86_64-xlate.pl" and -f $xlate) or
 die "can't locate x86_64-xlate.pl";
 
-open OUT,"| \"$^X\" $xlate $flavour $output";
-*STDOUT=*OUT;
+open STDOUT,"| $^X $xlate $flavour $output";
 
 ($lo,$hi)=("%rax","%rdx");	$a=$lo;
 ($i0,$i1)=("%rsi","%rdi");
