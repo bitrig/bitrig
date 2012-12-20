863dff1e2de219f26f6e231447f2e1294cc655be
David Hill <dhill@mindcry.org>
2012-12-20 14:35:50 -0500

    Fix i386 kernel breakage due to an unitialized variable.

--- sys/arch/i386/i386/trap.c
+++ sys/arch/i386/i386/trap.c
@@ -542,7 +542,7 @@ syscall(struct trapframe *frame)
 	caddr_t params;
 	struct sysent *callp;
 	struct proc *p;
-	int orig_error, error, opc, nsys;
+	int error, opc, nsys;
 	register_t code, args[8], rval[2];
 #ifdef DIAGNOSTIC
 	int ocpl = lapic_tpr;
@@ -643,7 +643,7 @@ syscall(struct trapframe *frame)
 	rval[0] = 0;
 	rval[1] = frame->tf_edx;
 
-	orig_error = error = mi_syscall(p, code, callp, args, rval);
+	error = mi_syscall(p, code, callp, args, rval);
 
 	switch (error) {
 	case 0:
@@ -671,7 +671,7 @@ syscall(struct trapframe *frame)
 		break;
 	}
 
-	mi_syscall_return(p, code, orig_error, rval);
+	mi_syscall_return(p, code, error, rval);
 
 #ifdef DIAGNOSTIC
 	if (lapic_tpr != ocpl) {
