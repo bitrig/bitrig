fcf7ac45319c8b442e064ef448d5cd2ecf32a56c
jasper <jasper@cvs.openbsd.org>
2013-01-01 19:39:51 +0000

    copyright++;
    OGA: bump bitrig copyright too

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.185 2012/11/05 19:39:34 miod Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.186 2013/01/01 19:39:50 jasper Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -107,8 +107,8 @@ extern void nfs_init(void);
 const char	copyright[] =
 "Copyright (c) 1982, 1986, 1989, 1991, 1993\n"
 "\tThe Regents of the University of California.  All rights reserved.\n"
-"Copyright (c) 1995-2012 OpenBSD. All rights reserved.  http://www.OpenBSD.org\n"
-"Copyright (c) 2012 Bitrig. All rights reserved.  http://www.bitrig.org\n";
+"Copyright (c) 1995-2013 OpenBSD. All rights reserved.  http://www.OpenBSD.org\n"
+"Copyright (c) 2012-2013 Bitrig. All rights reserved.  http://www.bitrig.org\n";
 
 /* Components of the first process -- never freed. */
 struct	session session0;
