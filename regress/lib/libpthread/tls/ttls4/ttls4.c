9ebf7f5179eb6168ad20b7259d453e799ff47b4a
Dale Rahn <drahn@dalerahn.com>
2013-01-28 15:30:14 -0500

    TLS test code from freebsd.

---
+++ regress/lib/libpthread/tls/ttls4/ttls4.c
@@ -0,0 +1,57 @@
+/*
+ * This program tests if a new thread's initial tls data
+ * is clean.
+ *
+ * David Xu <davidxu@freebsd.org>
+ *
+ * $FreeBSD$
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <machine/sysarch.h>
+
+int __thread n;
+int __thread m;
+int cnt;
+
+void *f1(void *arg)
+{
+	int *parg = arg;
+	if (n != 0) {
+		printf("\nbug, n == %d m == %d &n %p call %d \n", n, m, &n,
+		    *parg);
+	//	exit(1);
+	}
+	printf("\rcnt %05d", cnt);
+	fflush(stdout);
+	cnt++;
+	n = 1;
+	return (0);
+}
+
+int
+main(int argc, char **argv)
+{
+	pthread_t td;
+	int i;
+	int arg;
+
+	char **p;
+
+	n = 2;
+	for (i = 0; i < 1000; ++i) {
+		arg = 1;
+		pthread_create(&td, NULL, f1, &arg);
+		pthread_join(td, NULL);
+	}
+	sleep(2);
+	for (i = 0; i < 1000; ++i) {
+		arg = 2;
+		pthread_create(&td, NULL, f1, &arg);
+		pthread_join(td, NULL);
+	}
+	return (0);
+}
