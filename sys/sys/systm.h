11381f61f71f2c8f5a07d757857a384ca9ceec32
tedu <tedu@cvs.openbsd.org>
2013-04-06 03:53:26 +0000

    shuffle around some poison code, prototypes, values... allow some more pool debug code to be enabled if not compiled in bump poison size back up to 64

--- sys/sys/systm.h
+++ sys/sys/systm.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: systm.h,v 1.97 2013/04/06 03:44:34 tedu Exp $	*/
+/*	$OpenBSD: systm.h,v 1.98 2013/04/06 03:53:25 tedu Exp $	*/
 /*	$NetBSD: systm.h,v 1.50 1996/06/09 04:55:09 briggs Exp $	*/
 
 /*-
@@ -190,9 +190,6 @@ void	assertwaitok(void);
 #else
 #define	assertwaitok()	do { /* nothing */ } while (0)
 #endif
-
-void	poison_mem(void *, size_t);
-int	poison_check(void *, size_t, size_t *, int *);
 
 void	tablefull(const char *);
 
