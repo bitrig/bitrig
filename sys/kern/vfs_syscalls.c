bitrig/bitrig

commit cc73631cd444fbc1642d38fae4b44f56c1e1adb7

Do hardlink sanity checks in the syscall layer.
No hardlinks on directories or across mount points.
KASSERT() this in the filesystems.

sys/kern/vfs_syscalls.c
@@ -1580,16 +1580,36 @@ dolinkat(struct proc *p, int fd1, const char *path1, int fd2,
 	if ((error = namei(&nd)) != 0)
 		goto out;
 	if (nd.ni_vp) {
-		VOP_ABORTOP(nd.ni_dvp, &nd.ni_cnd);
-		if (nd.ni_dvp == nd.ni_vp)
-			vrele(nd.ni_dvp);
-		else
-			vput(nd.ni_dvp);
-		vrele(nd.ni_vp);
 		error = EEXIST;
+		goto abort;
+	}
+
+	/* Prevent hard links on directories. */
+	if (vp->v_type == VDIR) {
+		error = EPERM;
+		goto abort;
+	}
+	/* Prevent hardlinks across mount points */
+	if (nd.ni_dvp->v_mount != vp->v_mount) {
+		error = EXDEV;
+		goto abort;
+	}
+
+	error = VOP_LINK(nd.ni_dvp, vp, &nd.ni_cnd);
+	if (!error) {
 		goto out;
 	}
-	error = VOP_LINK(nd.ni_dvp, vp, &nd.ni_cnd);
+
+abort:
+	if (nd.ni_vp) {
+		VOP_ABORTOP(nd.ni_dvp, &nd.ni_cnd);
+		if (nd.ni_dvp == nd.ni_vp) {
+			vrele(nd.ni_dvp);
+		} else {
+			vput(nd.ni_dvp);
+		}
+		vrele(nd.ni_vp);
+	}
 out:
 	vrele(vp);
 	return (error);

sys/nfs/nfs_vnops.c
@@ -1682,14 +1682,8 @@ nfs_link(void *v)
 
 	info.nmi_v3 = NFS_ISV3(vp);
 
-	if (dvp->v_mount != vp->v_mount) {
-		pool_put(&namei_pool, cnp->cn_pnbuf);
-		if (vp == dvp)
-			vrele(dvp);
-		else
-			vput(dvp);
-		return (EXDEV);
-	}
+	KASSERT(vp->v_type != VDIR);
+	KASSERT(dvp->v_mount == vp->v_mount);
 
 	/*
 	 * Push all writes to the server, so that the attribute cache

sys/ufs/ext2fs/ext2fs_vnops.c
@@ -433,16 +433,10 @@ ext2fs_link(void *v)
 	if ((cnp->cn_flags & HASBUF) == 0)
 		panic("ext2fs_link: no name");
 #endif
-	if (vp->v_type == VDIR) {
-		VOP_ABORTOP(dvp, cnp);
-		error = EISDIR;
-		goto out2;
-	}
-	if (dvp->v_mount != vp->v_mount) {
-		VOP_ABORTOP(dvp, cnp);
-		error = EXDEV;
-		goto out2;
-	}
+
+	KASSERT(vp->v_type != VDIR);
+	KASSERT(dvp->v_mount == vp->v_mount);
+
 	if (dvp != vp && (error = vn_lock(vp, LK_EXCLUSIVE, p))) {
 		VOP_ABORTOP(dvp, cnp);
 		goto out2;

sys/ufs/ufs/ufs_vnops.c
@@ -588,16 +588,9 @@ ufs_link(void *v)
 	if ((cnp->cn_flags & HASBUF) == 0)
 		panic("ufs_link: no name");
 #endif
-	if (vp->v_type == VDIR) {
-		VOP_ABORTOP(dvp, cnp);
-		error = EPERM;
-		goto out2;
-	}
-	if (dvp->v_mount != vp->v_mount) {
-		VOP_ABORTOP(dvp, cnp);
-		error = EXDEV;
-		goto out2;
-	}
+	KASSERT(vp->v_type != VDIR);
+	KASSERT(dvp->v_mount == vp->v_mount);
+
 	if (dvp != vp && (error = vn_lock(vp, LK_EXCLUSIVE, p))) {
 		VOP_ABORTOP(dvp, cnp);
 		goto out2;

commit a023e13317dbe7776136908f01e1771a7f827d8a

fix regressions introduced in cc73631cd444fbc1642d38fae4b44f56c1e1adb7, noticed by drahn@ and jy-p@ while building gcc and by regress/sys/ffs; okay oga@

sys/kern/vfs_syscalls.c
@@ -1573,7 +1573,8 @@ dolinkat(struct proc *p, int fd1, const char *path1, int fd2,
 
 	flags = LOCKPARENT;
 	if (vp->v_type == VDIR) {
-		flags |= STRIPSLASHES;
+		vrele(vp);
+		return EPERM;
 	}
 
 	NDINITAT(&nd, CREATE, flags, UIO_USERSPACE, fd2, path2, p);
@@ -1584,11 +1585,6 @@ dolinkat(struct proc *p, int fd1, const char *path1, int fd2,
 		goto abort;
 	}
 
-	/* Prevent hard links on directories. */
-	if (vp->v_type == VDIR) {
-		error = EPERM;
-		goto abort;
-	}
 	/* Prevent hardlinks across mount points */
 	if (nd.ni_dvp->v_mount != vp->v_mount) {
 		error = EXDEV;
@@ -1596,22 +1592,21 @@ dolinkat(struct proc *p, int fd1, const char *path1, int fd2,
 	}
 
 	error = VOP_LINK(nd.ni_dvp, vp, &nd.ni_cnd);
-	if (!error) {
-		goto out;
-	}
-
-abort:
-	if (nd.ni_vp) {
-		VOP_ABORTOP(nd.ni_dvp, &nd.ni_cnd);
-		if (nd.ni_dvp == nd.ni_vp) {
-			vrele(nd.ni_dvp);
-		} else {
-			vput(nd.ni_dvp);
-		}
-		vrele(nd.ni_vp);
-	}
 out:
 	vrele(vp);
+	return (error);
+
+abort:
+	VOP_ABORTOP(nd.ni_dvp, &nd.ni_cnd);
+	if (nd.ni_dvp == nd.ni_vp) {
+		vrele(nd.ni_dvp);
+	} else {
+		vput(nd.ni_dvp);
+	}
+	if (nd.ni_vp)
+		vrele(nd.ni_vp);
+	vrele(vp);
+
 	return (error);
 }
 