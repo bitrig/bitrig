043e8ee91bcac179868b7bbbbdde309bfd273158
Patrick Wildt <patrick@blueri.se>
2013-06-10 16:22:00 +0200

    Make compose_tab_cmp a static function, else clang 3.3 doesn't inline it properly using -Oz.

--- sys/dev/wscons/wskbdutil.c
+++ sys/dev/wscons/wskbdutil.c
@@ -177,11 +177,11 @@ static struct compose_tab_s {
 
 static int compose_tab_inorder = 0;
 
-inline int compose_tab_cmp(struct compose_tab_s *, struct compose_tab_s *);
+static inline int compose_tab_cmp(struct compose_tab_s *, struct compose_tab_s *);
 keysym_t ksym_upcase(keysym_t);
 void fillmapentry(const keysym_t *, int, struct wscons_keymap *);
 
-inline int
+static inline int
 compose_tab_cmp(struct compose_tab_s *i, struct compose_tab_s *j)
 {
 	if (i->elem[0] == j->elem[0])
