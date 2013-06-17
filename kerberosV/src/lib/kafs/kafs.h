3b39266ba17ef0ca2b8361b6ccb2e17264b7c319
robert <robert@cvs.openbsd.org>
2013-06-17 18:57:47 +0000

    update kerberosV to Heimdal 1.5.2 using BSD Makefiles based on the ones from FreeBSD
    This is getting commited now so that we can patch it up
    fast in the tree.
    There are known regressions so please don't try to build
    this on your own now, please wait for the next available
    snapshot.
    
    If however you would still want to build it, make sure
    build and install everything in the tools directory first.
    
    This is not the full commit yet because there are several
    subdirectories.

--- kerberosV/src/lib/kafs/kafs.h
+++ kerberosV/src/lib/kafs/kafs.h
@@ -1,23 +1,23 @@
 /*
- * Copyright (c) 1995 - 2001, 2003 Kungliga Tekniska Högskolan
+ * Copyright (c) 1995 - 2001, 2003 Kungliga Tekniska HÃ¶gskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the Institute nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  */
 
-/* $KTH: kafs.h,v 1.43 2005/02/03 08:45:13 lha Exp $ */
+/* $Id: kafs.h,v 1.11 2013/06/17 18:57:43 robert Exp $ */
 
 #ifndef __KAFS_H
 #define __KAFS_H
@@ -41,75 +41,14 @@
 /* sys/ioctl.h must be included manually before kafs.h */
 
 /*
- * The following parts are from the old nnpfs_pioct.h file
  */
-
-/* XXX: HORRID GRUESOME HACK FOR AFS / KERBEROS */
-
-/*	$OpenBSD: nnpfs_pioctl.h,v 1.1 2009/06/03 14:45:54 jj Exp $	*/
-/*
- * Copyright (c) 1995 - 2000 Kungliga Tekniska Högskolan
- * (Royal Institute of Technology, Stockholm, Sweden).
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the Kungliga Tekniska
- *      Högskolan and its contributors.
- *
- * 4. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-#ifndef	_NNPFS_NNPFS_PIOCTL_H_
-#define	_NNPFS_NNPFS_PIOCTL_H_
-
-/*
- */
-
 #define AFSCALL_PIOCTL 20
 #define AFSCALL_SETPAG 21
-#define AFSCALL_PROBE  22
 
 #ifndef _VICEIOCTL
 #define _VICEIOCTL(id)  ((unsigned int ) _IOW('V', id, struct ViceIoctl))
+#define _AFSCIOCTL(id)  ((unsigned int ) _IOW('C', id, struct ViceIoctl))
 #endif /* _VICEIOCTL */
-
-/*
- * ioctls
- */
-
-#define VIOCCLOSEWAIT		_VICEIOCTL(1)
-#define VIOCABORT		_VICEIOCTL(2)
-#define VIOIGETCELL		_VICEIOCTL(3)
-
-/*
- * pioctls
- */
 
 #define VIOCSETAL		_VICEIOCTL(1)
 #define VIOCGETAL		_VICEIOCTL(2)
@@ -117,23 +56,15 @@
 #define VIOCGETVOLSTAT		_VICEIOCTL(4)
 #define VIOCSETVOLSTAT		_VICEIOCTL(5)
 #define VIOCFLUSH		_VICEIOCTL(6)
-#define VIOCSTAT		_VICEIOCTL(7)
 #define VIOCGETTOK		_VICEIOCTL(8)
 #define VIOCUNLOG		_VICEIOCTL(9)
 #define VIOCCKSERV		_VICEIOCTL(10)
 #define VIOCCKBACK		_VICEIOCTL(11)
 #define VIOCCKCONN		_VICEIOCTL(12)
-#define VIOCGETTIME		_VICEIOCTL(13)
 #define VIOCWHEREIS		_VICEIOCTL(14)
-#define VIOCPREFETCH		_VICEIOCTL(15)
-#define VIOCNOP			_VICEIOCTL(16)
-#define VIOCENGROUP		_VICEIOCTL(17)
-#define VIOCDISGROUP		_VICEIOCTL(18)
-#define VIOCLISTGROUPS		_VICEIOCTL(19)
 #define VIOCACCESS		_VICEIOCTL(20)
 #define VIOCUNPAG		_VICEIOCTL(21)
 #define VIOCGETFID		_VICEIOCTL(22)
-#define VIOCWAITFOREVER		_VICEIOCTL(23)
 #define VIOCSETCACHESIZE	_VICEIOCTL(24)
 #define VIOCFLUSHCB		_VICEIOCTL(25)
 #define VIOCNEWCELL		_VICEIOCTL(26)
@@ -151,112 +82,30 @@
 #define VIOC_AFS_SYSNAME	_VICEIOCTL(38)
 #define VIOC_EXPORTAFS		_VICEIOCTL(39)
 #define VIOCGETCACHEPARAMS	_VICEIOCTL(40)
-#define VIOCCONNECTMODE		_VICEIOCTL(41)
-#define VIOCGETVCXSTATUS	_VICEIOCTL(41)
-#define VIOC_SETSPREFS33	_VICEIOCTL(42)
-#define VIOC_GETSPREFS		_VICEIOCTL(43)
-#define VIOC_GAG		_VICEIOCTL(44)
-#define VIOC_TWIDDLE		_VICEIOCTL(45)
-#define VIOC_SETSPREFS		_VICEIOCTL(46)
-#define VIOC_STORBEHIND		_VICEIOCTL(47)
 #define VIOC_GCPAGS		_VICEIOCTL(48)
-#define VIOC_GETINITPARAMS      _VICEIOCTL(49)
-#define VIOC_GETCPREFS          _VICEIOCTL(50)
-#define VIOC_SETCPREFS          _VICEIOCTL(51)
-#define VIOC_FLUSHMOUNT         _VICEIOCTL(52)
-#define VIOC_RXSTATPROC         _VICEIOCTL(53)
-#define VIOC_RXSTATPEER         _VICEIOCTL(54)
 
-#define VIOC_GETRXKCRYPT	_VICEIOCTL(55)
-#define VIOC_SETRXKCRYPT	_VICEIOCTL(56)
-
-/* arla specific */
-
-#define VIOC_FPRIOSTATUS	_VICEIOCTL(57)
-#define VIOC_FHGET		_VICEIOCTL(58)
-#define VIOC_FHOPEN		_VICEIOCTL(59)
-#define VIOC_NNPFSDEBUG         _VICEIOCTL(60)
-#define VIOC_NNPFSDEBUG           _VICEIOCTL(60)
-#define VIOC_ARLADEBUG		_VICEIOCTL(61)
-#define VIOC_AVIATOR            _VICEIOCTL(62)
-#define VIOC_NNPFSDEBUG_PRINT	_VICEIOCTL(63)
-#define VIOC_CALCULATE_CACHE	_VICEIOCTL(64)
-#define VIOC_BREAKCALLBACK	_VICEIOCTL(65)
-#define VIOC_PREFETCHTAPE       _VICEIOCTL(66)
-#define VIOC_RESIDENCY_CMD      _VICEIOCTL(67)
-
-/*
- * GETCELLSTATUS flags
- */
-
-#define CELLSTATUS_PRIMARY	0x01 /* this is the `primary' cell */
-#define CELLSTATUS_SETUID	0x02 /* setuid honored for this cell */
-#define CELLSTATUS_OBSOLETE_VL	0x04 /* uses obsolete VL servers */
-
-/*
- * VIOCCONNECTMODE arguments
- */
-
-#define CONNMODE_PROBE		0
-#define CONNMODE_CONN		1
-#define CONNMODE_FETCH		2
-#define CONNMODE_DISCONN	3
-#define CONNMODE_PARCONNECTED	4
-
-/*
- * The struct for VIOC_FPRIOSTATUS
- */
-
-#define FPRIO_MAX		100
-#define FPRIO_MIN		0
-#define FPRIO_DEFAULT		FPRIO_MAX
-
-#define FPRIO_GET		0
-#define FPRIO_SET		1
-#define FPRIO_GETMAX		2
-#define FPRIO_SETMAX		3
-
-struct vioc_fprio {
-	int16_t cmd;
-	int16_t prio;
-	int32_t Cell;
-	int32_t Volume;
-	int32_t Vnode;
-	int32_t Unique;
-};
-
-/*
- * Flags for VIOCCKSERV
- */
-
-#define CKSERV_DONTPING     1
-#define CKSERV_FSONLY       2
-
-#define CKSERV_MAXSERVERS   16 /* limitation of VIOCCKSERV number of 
-				  returned servers */
+#define VIOCGETTOK2		_AFSCIOCTL(7)
+#define VIOCSETTOK2		_AFSCIOCTL(8)
 
 struct ViceIoctl {
-	caddr_t	in, out;
-	short	in_size;
-	short	out_size;
+  caddr_t in, out;
+  unsigned short in_size;
+  unsigned short out_size;
 };
 
 struct ClearToken {
-	int32_t	AuthHandle;
-	char	HandShakeKey[8];
-	int32_t	ViceId;
-	int32_t	BeginTimestamp;
-	int32_t	EndTimestamp;
+  int32_t AuthHandle;
+  char HandShakeKey[8];
+  int32_t ViceId;
+  int32_t BeginTimestamp;
+  int32_t EndTimestamp;
 };
-
-#endif
-
-/* XXX: END HORRID GRUESOME HACK FOR AFS / KERBEROS */
 
 /* Use k_hasafs() to probe if the machine supports AFS syscalls.
    The other functions will generate a SIGSYS if AFS is not supported */
 
 int k_hasafs (void);
+int k_hasafs_recheck (void);
 
 int krb_afslog (const char *cell, const char *realm);
 int krb_afslog_uid (const char *cell, const char *realm, uid_t uid);
@@ -308,7 +157,7 @@ krb5_error_code krb5_afslog_uid (krb5_context context,
 				     krb5_const_realm realm,
 				     uid_t uid);
 krb5_error_code krb5_afslog (krb5_context context,
-				 krb5_ccache id, 
+				 krb5_ccache id,
 				 const char *cell,
 				 krb5_const_realm realm);
 krb5_error_code krb5_afslog_uid_home (krb5_context context,
@@ -328,22 +177,21 @@ krb5_error_code krb5_realm_of_cell (const char *cell, char **realm);
 
 #endif
 
-#define _PATH_VICE		"/etc/afs/"
+
+#define _PATH_VICE		"/usr/vice/etc/"
 #define _PATH_THISCELL 		_PATH_VICE "ThisCell"
 #define _PATH_CELLSERVDB 	_PATH_VICE "CellServDB"
 #define _PATH_THESECELLS	_PATH_VICE "TheseCells"
 
-#define _PATH_ARLA_VICE		"/etc/afs/"
+#define _PATH_ARLA_VICE		"/usr/arla/etc/"
 #define _PATH_ARLA_THISCELL	_PATH_ARLA_VICE "ThisCell"
 #define _PATH_ARLA_CELLSERVDB 	_PATH_ARLA_VICE "CellServDB"
 #define _PATH_ARLA_THESECELLS	_PATH_ARLA_VICE "TheseCells"
 
-#if 0
 #define _PATH_OPENAFS_DEBIAN_VICE		"/etc/openafs/"
 #define _PATH_OPENAFS_DEBIAN_THISCELL		_PATH_OPENAFS_DEBIAN_VICE "ThisCell"
 #define _PATH_OPENAFS_DEBIAN_CELLSERVDB 	_PATH_OPENAFS_DEBIAN_VICE "CellServDB"
 #define _PATH_OPENAFS_DEBIAN_THESECELLS		_PATH_OPENAFS_DEBIAN_VICE "TheseCells"
-#endif
 
 #define _PATH_OPENAFS_MACOSX_VICE		"/var/db/openafs/etc/"
 #define _PATH_OPENAFS_MACOSX_THISCELL		_PATH_OPENAFS_MACOSX_VICE "ThisCell"
