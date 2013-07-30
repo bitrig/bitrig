f907771709d21e2314cb017e7f6ca0f1692cb87d
beck <beck@cvs.openbsd.org>
2013-07-30 17:07:57 +0000

    The previous change was made while chasing nfs performance issues on Theo's servers - however this was in the context of the buffer flipper changes and this is now suspect in a continues performance issue with NFS so back it out for now

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_subr.c,v 1.204 2013/06/24 18:52:37 beck Exp $	*/
+/*	$OpenBSD: vfs_subr.c,v 1.205 2013/07/30 17:07:56 beck Exp $	*/
 /*	$NetBSD: vfs_subr.c,v 1.53 1996/04/22 01:39:13 christos Exp $	*/
 
 /*
@@ -1814,7 +1814,6 @@ vinvalbuf(struct vnode *vp, int flags, struct ucred *cred, struct proc *p,
 		panic("vinvalbuf(): vp isn't locked");
 #endif
 
-loop:
 	if (flags & V_SAVE) {
 		s = splbio();
 		vwaitforio(vp, 0, "vinvalbuf", 0);
@@ -1829,6 +1828,7 @@ loop:
 		}
 		splx(s);
 	}
+loop:
 	s = splbio();
 	for (;;) {
 		if ((blist = LIST_FIRST(&vp->v_cleanblkhd)) &&
@@ -1851,13 +1851,24 @@ loop:
 				bp->b_flags |= B_WANTED;
 				error = tsleep(bp, slpflag | (PRIBIO + 1),
 				    "vinvalbuf", slptimeo);
-				splx(s);
 				if (error) {
+					splx(s);
 					return (error);
 				}
-				goto loop;
+				break;
 			}
 			bremfree(bp);
+			/*
+			 * XXX Since there are no node locks for NFS, I believe
+			 * there is a slight chance that a delayed write will
+			 * occur while sleeping just above, so check for it.
+			 */
+			if ((bp->b_flags & B_DELWRI) && (flags & V_SAVE)) {
+				buf_acquire(bp);
+				splx(s);
+				(void) VOP_BWRITE(bp);
+				goto loop;
+			}
 			buf_acquire_nomap(bp);
 			bp->b_flags |= B_INVAL;
 			brelse(bp);
