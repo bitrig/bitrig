bb7f53d540434bebdd0588a4d8433f2a1e944046
patrick <patrick@cvs.openbsd.org>
2013-08-28 19:26:06 +0000

    Following the ARM manuals and cookbooks, ldrex/strex don't have implicit barriers.  Therefore it is required to add a memory berrier, so that it is ensured that all observers observed the change.
    ok rapha@

--- lib/librthread/arch/arm/_atomic_lock.c
+++ lib/librthread/arch/arm/_atomic_lock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: _atomic_lock.c,v 1.5 2013/06/02 19:08:09 maja Exp $	*/
+/*	$OpenBSD: _atomic_lock.c,v 1.6 2013/08/28 19:26:05 patrick Exp $	*/
 
 /*
  * Copyright (c) 2004 Dale Rahn. All rights reserved.
