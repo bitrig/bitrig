28e937de1d34acb2064411d22ce3e0b549bcb4c4
pedro martelletto <pedro@ambientworks.net>
2013-10-13 13:19:38 +0200

    Remove dead libc MD bits and pieces
    Remove machine-dependent libc code for architectures we do not support.
    
    OK drahn@ oga@

--- lib/libc/arch/hppa/DEFS.h
+++
@@ -1,3 +0,0 @@
-/*	$OpenBSD: DEFS.h,v 1.1 1998/08/28 20:59:40 mickey Exp $	*/
-
-#include <machine/asm.h>
--- lib/libc/arch/vax/DEFS.h
+++
@@ -1,4 +0,0 @@
-/*	$OpenBSD: DEFS.h,v 1.4 1996/08/19 08:18:17 tholo Exp $ */
-
-#include <machine/asm.h>
-
