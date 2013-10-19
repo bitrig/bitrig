7cf9767cfbd8dee3e98590101ce3df4789bfb0f7
krw <krw@cvs.openbsd.org>
2013-10-19 09:32:16 +0000

    Use daddr_t * instead of int * for the partoffp parameter to readdoslabel(). Ditto all the MD variables whose addresses are passed to readdoslabel() via partoffp.
    Fix some 512-byte block vs disk sector confusion in hppa and sgi.
    All the DL_GETxxxxx() defines return disk sector values. All
    DL_SETxxxx() take disk sector values. These changes should be no-ops
    until a drive using non-512-byte-sectors is encountered.
    
    ok deraadt@

--- sys/kern/subr_disk.c
+++ sys/kern/subr_disk.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_disk.c,v 1.154 2013/10/14 23:35:53 krw Exp $	*/
+/*	$OpenBSD: subr_disk.c,v 1.155 2013/10/19 09:32:15 krw Exp $	*/
 /*	$NetBSD: subr_disk.c,v 1.17 1996/03/16 23:17:08 christos Exp $	*/
 
 /*
@@ -292,7 +292,7 @@ checkdisklabel(void *rlp, struct disklabel *lp, u_int64_t boundstart,
  */
 int
 readdoslabel(struct buf *bp, void (*strat)(struct buf *),
-    struct disklabel *lp, int *partoffp, int spoofonly)
+    struct disklabel *lp, daddr_t *partoffp, int spoofonly)
 {
 	u_int64_t dospartoff = 0, dospartend = DL_GETBEND(lp);
 	int i, ourpart = -1, wander = 1, n = 0, loop = 0, offset;
@@ -536,7 +536,7 @@ notfat:
  */
 int
 readgptlabel(struct buf *bp, void (*strat)(struct buf *),
-    struct disklabel *lp, int *partoffp, int spoofonly)
+    struct disklabel *lp, daddr_t *partoffp, int spoofonly)
 {
 	u_int64_t gptpartoff = 0, gptpartend = DL_GETBEND(lp);
 	int i, ourpart = -1, offset, n = 0;
