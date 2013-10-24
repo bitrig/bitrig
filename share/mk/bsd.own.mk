37d13320fee189089ac8b120bab9acaf2d970852
Dale Rahn <drahn@dalerahn.com>
2013-10-24 18:49:26 -0400

    Not yet ready to have clang be default cc/CC

--- share/mk/bsd.own.mk
+++ share/mk/bsd.own.mk
@@ -194,7 +194,7 @@ MK_INSTALLLIB?=yes
 MK_CLANG_EXTRAS?=No
 
 # MK_CLANG_IS_CC controls if clang should be installed as /usr/bin/cc
-MK_CLANG_IS_CC?=Yes
+MK_CLANG_IS_CC?=No
 
 # MK_SHARED_TOOLCHAIN forces compiler toolchain to build static (if set to no).
 MK_SHARED_TOOLCHAIN?=Yes
