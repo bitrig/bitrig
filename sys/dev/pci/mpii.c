cb49967bbdf76890fa20222f946e1c5eb8a53ec7
dlg <dlg@cvs.openbsd.org>
2013-11-06 08:00:18 +0000

    dont leak ccbs in mpii_sas_remove_device. detaching lots (like, 1000) of things could run you out of command slots without this.

--- sys/dev/pci/mpii.c
+++ sys/dev/pci/mpii.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mpii.c,v 1.69 2013/01/25 04:25:21 dlg Exp $	*/
+/*	$OpenBSD: mpii.c,v 1.71 2013/11/06 08:00:17 dlg Exp $	*/
 /*
  * Copyright (c) 2010, 2012 Mike Belopuhov
  * Copyright (c) 2009 James Giannoules
@@ -1897,6 +1897,8 @@ mpii_sas_remove_device(struct mpii_softc *sc, u_int16_t handle)
 	mpii_wait(sc, ccb);
 	if (ccb->ccb_rcb != NULL)
 		mpii_push_reply(sc, ccb->ccb_rcb);
+
+	scsi_io_put(&sc->sc_iopool, ccb);
 }
 
 int
