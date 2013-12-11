95f8621f46fa5294d7042290cd3f99bf0505c13e
pedro martelletto <pedro@ambientworks.net>
2013-12-11 15:02:39 +0100

    remove tmpfs_getpages/tmpfs_putpages; ok oga@

--- sys/tmpfs/tmpfs_vnops.h
+++ sys/tmpfs/tmpfs_vnops.h
@@ -77,7 +77,5 @@ int	tmpfs_print		(void *);
 int	tmpfs_pathconf		(void *);
 int	tmpfs_advlock		(void *);
 int	tmpfs_bwrite		(void *);
-int	tmpfs_getpages		(void *);
-int	tmpfs_putpages		(void *);
 
 #endif /* _TMPFS_TMPFS_VNOPS_H_ */
