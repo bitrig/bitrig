064d94edb864b65ea66845447ebd78ebec71734c
deraadt <deraadt@cvs.openbsd.org>
2013-12-28 03:22:53 +0000

    change the stack protector guard into a long word (removing the old legacy compat pointed out by miod), and place it inside the ELF .openbsd.randomdata segment.  Inside main(), only re-initialize the guard if the bootblocks failed to initialize it for us.
    OGA: and do __guard too, our clang doesn't handle __guard_local

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: init_main.c,v 1.194 2013/12/28 03:12:56 deraadt Exp $	*/
+/*	$OpenBSD: init_main.c,v 1.195 2013/12/28 03:22:52 deraadt Exp $	*/
 /*	$NetBSD: init_main.c,v 1.84.4.1 1996/06/02 09:08:06 mrg Exp $	*/
 
 /*
@@ -133,10 +133,8 @@ int	ncpusfound = 1;			/* number of cpus we find */
 __volatile int start_init_exec;		/* semaphore for start_init() */
 
 #if !defined(NO_PROPOLICE)
-long	__guard[8] = { 0 };
-#ifdef __ELF__
-long	__guard_local __dso_hidden;
-#endif
+long	__guard __attribute__((section(".openbsd.randomdata")));
+long	__guard_local __attribute__((section(".openbsd.randomdata")));
 #if !defined(__clang__) || (!(__clang_major__ == 3 && __clang_minor__ < 2))
 extern
 #endif
@@ -420,16 +418,17 @@ main(void *framep)
 #endif
 
 #if !defined(NO_PROPOLICE)
-	{
-		volatile long newguard[8];
+	if (__guard == 0) {
+		volatile long newguard;
 
-		arc4random_buf((long *)newguard, sizeof(newguard));
+		newguard = arc4random();
+		__guard = newguard;
+	}
+	if (__guard_local == 0) {
+		volatile long newguard;
 
-#ifdef __ELF__
-		__guard_local = newguard[0];
-#endif
-		for (i = nitems(__guard) - 1; i; i--)
-			__guard[i] = newguard[i];
+		newguard = arc4random();
+		__guard_local = newguard;
 	}
 #endif
 
