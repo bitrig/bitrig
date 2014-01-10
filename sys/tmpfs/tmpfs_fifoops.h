18a68bb6e0834841bd98337689f03d597a79baf3
pedro martelletto <pedro@ambientworks.net>
2014-01-10 11:24:30 +0100

    kill tmpfs_fifo_*; inspired by OpenBSD
    no binary change

--- sys/tmpfs/tmpfs_fifoops.h
+++ sys/tmpfs/tmpfs_fifoops.h
@@ -47,40 +47,9 @@
 
 extern struct vops tmpfs_fifovops;
 
-#define	tmpfs_fifo_lookup	vop_generic_lookup
-#define	tmpfs_fifo_create	fifo_badop
-#define	tmpfs_fifo_mknod	fifo_badop
-#define	tmpfs_fifo_open		fifo_open
 int	tmpfs_fifo_close	(void *);
-#define	tmpfs_fifo_access	tmpfs_access
-#define	tmpfs_fifo_getattr	tmpfs_getattr
-#define	tmpfs_fifo_setattr	tmpfs_setattr
 int	tmpfs_fifo_read		(void *);
 int	tmpfs_fifo_write	(void *);
-#define	tmpfs_fifo_ioctl	fifo_ioctl
-#define	tmpfs_fifo_poll		fifo_poll
-#define	tmpfs_fifo_kqfilter	fifo_kqfilter
-#define	tmpfs_fifo_revoke	vop_generic_revoke
 int	tmpfs_fifo_fsync	(void *);
-#define	tmpfs_fifo_remove	fifo_badop
-#define	tmpfs_fifo_link		fifo_badop
-#define	tmpfs_fifo_rename	fifo_badop
-#define	tmpfs_fifo_mkdir	fifo_badop
-#define	tmpfs_fifo_rmdir	fifo_badop
-#define	tmpfs_fifo_symlink	fifo_badop
-#define	tmpfs_fifo_readdir	fifo_badop
-#define	tmpfs_fifo_readlink	fifo_badop
-#define	tmpfs_fifo_abortop	fifo_badop
-#define	tmpfs_fifo_inactive	tmpfs_inactive
-#define	tmpfs_fifo_reclaim	tmpfs_reclaim
-#define	tmpfs_fifo_lock		tmpfs_lock
-#define	tmpfs_fifo_unlock	tmpfs_unlock
-#define	tmpfs_fifo_bmap		vop_generic_bmap
-#define	tmpfs_fifo_strategy	fifo_badop
-#define	tmpfs_fifo_print	tmpfs_print
-#define	tmpfs_fifo_pathconf	fifo_pathconf
-#define	tmpfs_fifo_islocked	tmpfs_islocked
-#define	tmpfs_fifo_advlock	fifo_advlock
-#define	tmpfs_fifo_bwrite	tmpfs_bwrite
 
 #endif /* _FS_TMPFS_TMPFS_FIFOOPS_H_ */
