a9f5445cfd9bc6fb883b9007303958d04708e3bc
Dale Rahn <drahn@dalerahn.com>
2014-01-19 01:13:59 -0500

    Arm needs __clear_cache, so provide it.

--- contrib/compiler-rt/lib/clear_cache.c
+++ contrib/compiler-rt/lib/clear_cache.c
@@ -10,8 +10,28 @@
 
 #include "int_lib.h"
 
+
 #if __APPLE__
   #include <libkern/OSCacheControl.h>
+#endif
+
+#if defined(__OpenBSD__) || defined(__Bitrig__)
+# if defined(__arm__) 
+#include <machine/asm.h>
+#include <sys/syscall.h>
+#include <arm/swi.h>
+
+static inline void
+sysarch (int32_t sysarch_id, void *arg) {
+	__asm volatile(" mov r0, %0;"
+	    " mov r1, %1;"
+	    " ldr r12, =%2;"
+	    " swi %3" ::
+	    "r" (sysarch_id), "r" (arg), "i"(SYS_sysarch), "i"(SYS_sysarch|SWI_OS_NETBSD):
+	    "0", "1");
+								    
+}
+# endif
 #endif
 
 /*
@@ -23,7 +43,7 @@
 
 void __clear_cache(void* start, void* end)
 {
-#if __i386__ || __x86_64__
+#if defined(__i386__) || defined(__x86_64__)
 /*
  * Intel processors have a unified instruction and data cache
  * so there is nothing to do
@@ -32,6 +52,14 @@ void __clear_cache(void* start, void* end)
     #if __APPLE__
         /* On Darwin, sys_icache_invalidate() provides this functionality */
         sys_icache_invalidate(start, end-start);
+    #elif (defined(__OpenBSD__) || defined(__Bitrig__)) && defined(__arm__)
+    	struct {
+		unsigned int addr;
+		int          len;
+	} s;
+	s.addr = (unsigned int)(start);
+	s.len = (end) - (start);
+	(void) sysarch (0, &s);   
     #else
         compilerrt_abort();
     #endif
