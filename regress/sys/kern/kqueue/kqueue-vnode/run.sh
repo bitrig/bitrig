7fbe6803f0f50b423d7b203ea2d1deb81b4a589d
pedro martelletto <pedro@ambientworks.net>
2014-01-23 22:48:17 +0100

    add directory tests; work in progress

--- regress/sys/kern/kqueue/kqueue-vnode/run.sh
+++ regress/sys/kern/kqueue/kqueue-vnode/run.sh
@@ -9,4 +9,15 @@ cd $1 && {
 	./kqtest x NOTE_ATTRIB dd if=/dev/zero of=x bs=1 seek=100 count=0
 	./kqtest x NOTE_REVOKE ./revoke x
 	./kqtest x NOTE_DELETE rm x
+	./kqtest -d x NOTE_WRITE touch x/x1
+	./kqtest -d x NOTE_WRITE,NOTE_LINK mkdir x/x2
+	touch xx
+	./kqtest -d x NOTE_WRITE ln xx x/x3
+	./kqtest -d x NOTE_WRITE rm -rf x/x1
+	./kqtest -d x NOTE_WRITE,NOTE_LINK rm -rf x/x2
+	./kqtest -d x NOTE_WRITE rm -rf x/x3
+	./kqtest -d x NOTE_REVOKE ./revoke x
+	./kqtest -d x NOTE_RENAME mv x y
+#	./kqtest -d x NOTE_ATTRIB chmod 0 y
+#	./kqtest -d x NOTE_DELETE rm -rf y
 }
