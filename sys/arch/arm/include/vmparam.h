9bdf7a8be1e597cb18848f024fa52984f795b671
Dale Rahn <drahn@dalerahn.com>
2014-02-08 11:51:27 -0500

    Restart vmparam.h by copying from i386 and adjusting it for arm.

--- sys/arch/arm/include/vmparam.h
+++ sys/arch/arm/include/vmparam.h
@@ -1,11 +1,9 @@
-/*	$OpenBSD: vmparam.h,v 1.15 2015/05/10 15:56:28 jsg Exp $	*/
-/*	$NetBSD: vmparam.h,v 1.18 2003/05/21 18:04:44 thorpej Exp $	*/
-
-/*
- * Copyright (c) 2001, 2002 Wasabi Systems, Inc.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Written by Jason R. Thorpe for Wasabi Systems, Inc.
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -15,83 +13,107 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed for the NetBSD Project by
- *	Wasabi Systems, Inc.
- * 4. The name of Wasabi Systems, Inc. may not be used to endorse
- *    or promote products derived from this software without specific prior
- *    written permission.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY WASABI SYSTEMS, INC. ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL WASABI SYSTEMS, INC
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)vmparam.h	5.9 (Berkeley) 5/12/91
  */
 
-#ifndef _ARM_VMPARAM_H_
-#define	_ARM_VMPARAM_H_
+#ifndef _MACHINE_VMPARAM_H_
+#define _MACHINE_VMPARAM_H_
+/*
+ * Machine dependent constants for arm.
+ */
 
 /*
- * Virtual Memory parameters common to all arm32 platforms.
+ * Virtual address space arrangement. On arm, both user and kernel
+ * share the address space, not unlike the vax.
+ * USRTEXT is the start of the user text/data space, while USRSTACK
+ * is the top (end) of the user stack. Immediately above the user stack
+ * resides the user structure, which is UPAGES long and contains the
+ * kernel stack.
+ *
+ * Immediately after the user structure is the page table map, and then
+ * kernel address space.
  */
-
-#define	USRTEXT		VM_MIN_ADDRESS
+#define	USRTEXT		PAGE_SIZE
 #define	USRSTACK	VM_MAXUSER_ADDRESS
-#define	KERNBASE	VM_MAXUSER_ADDRESS
 
 /*
- * Note that MAXTSIZ can't be larger than 32M, otherwise the compiler
- * would have to be changed to not generate "bl" instructions.
+ * Virtual memory related constants, all in bytes
  */
-#define	MAXTSIZ		(256*1024*1024)		/* max text size */
-#ifndef	DFLDSIZ
-#define	DFLDSIZ		(128*1024*1024)		/* initial data size limit */
+#define	MAXTSIZ		(128*1024*1024)		/* max text size */
+#ifndef DFLDSIZ
+#define	DFLDSIZ		(64*1024*1024)		/* initial data size limit */
 #endif
-#ifndef	MAXDSIZ
-#define	MAXDSIZ		(512*1024*1024)		/* max data size */
+#ifndef MAXDSIZ
+#define	MAXDSIZ		(1UL*1024*1024*1024)	/* max data size */
 #endif
 #ifndef BRKSIZ
-#define	BRKSIZ		MAXDSIZ			/* heap gap size */
+#define	BRKSIZ		(1024*1024*1024)	/* heap gap size */
 #endif
 #ifndef	DFLSSIZ
-#define	DFLSSIZ		(2*1024*1024)		/* initial stack size limit */
+#define	DFLSSIZ		(4*1024*1024)		/* initial stack size limit */
 #endif
 #ifndef	MAXSSIZ
-#define	MAXSSIZ		(8*1024*1024)		/* max stack size */
+#define	MAXSSIZ		(32*1024*1024)		/* max stack size */
 #endif
 
 #define STACKGAP_RANDOM	256*1024
 
 /*
- * Size of SysV shared memory map
+ * Size of shared memory map
  */
 #ifndef SHMMAXPGS
-#define	SHMMAXPGS	1024
+#define SHMMAXPGS	8192
 #endif
 
 /*
- * While the ARM architecture defines Section mappings, large pages,
- * and small pages, the standard page size is (and will always be) 4K.
+ * Specific addresses being unmapped and used as fillers for free memory.
  */
-#define	PAGE_SHIFT	12
-#define	PAGE_SIZE	(1 << PAGE_SHIFT)	/* bytes/page */
-#define	PAGE_MASK	(PAGE_SIZE - 1)
+#define	DEADBEEF0	0xefffeecc	/* malloc's filler */
+#define	DEADBEEF1	0xefffaabb	/* pool's filler */
 
-/*
- * Mach derived constants
- */
-#define	VM_MIN_ADDRESS		((vaddr_t) PAGE_SIZE)
-#define	VM_MAXUSER_ADDRESS	((vaddr_t) ARM_KERNEL_BASE)
-#define	VM_MAX_ADDRESS		VM_MAXUSER_ADDRESS
 
-#define	VM_MIN_KERNEL_ADDRESS	((vaddr_t) ARM_KERNEL_BASE)
+/* user/kernel map constants */
+#define VM_MIN_ADDRESS		((vaddr_t)PAGE_SIZE)
+#define VM_MAXUSER_ADDRESS	((vaddr_t)((PDSLOT_PTE<<PDSHIFT) - (2 * PAGE_SIZE)))
+#define VM_MAX_ADDRESS		((vaddr_t)((PDSLOT_PTE<<PDSHIFT) + \
+				    (PDSLOT_PTE<<PAGE_SHIFT)))
+#define VM_MIN_KERNEL_ADDRESS	((vaddr_t)KERNBASE)
+#define VM_MAX_KERNEL_ADDRESS	(0xffffffff)
 
-#endif /* _ARM_VMPARAM_H_ */
+/* virtual sizes (bytes) for various kernel submaps */
+#define VM_PHYS_SIZE		(USRIOSIZE*PAGE_SIZE)
+
+#define	VM_PHYSSEG_MAX	4	/* actually we could have this many segments */
+#define	VM_PHYSSEG_STRAT	VM_PSTRAT_BSEARCH
+#define	VM_PHYSSEG_NOADD	/* can't add RAM after vm_mem_init */
+
+#define __HAVE_VM_PAGE_MD
+struct pv_entry;
+struct vm_page_md {
+	LIST_HEAD(,pte_desc) pv_list;
+	int pvh_attrs;				/* page attributes */
+};
+
+#define VM_MDPAGE_INIT(pg) do {				\
+	LIST_HEAD_INITIALIZER((pg)->mdpage.pv_list);	\
+	(pg)->mdpage.pvh_attrs = 0;			\
+
+} while (0)
+
+#endif /* _MACHINE_VMPARAM_H_ */
