31893a2926380ce39bc29e66cb02133b047c17c0
halex <halex@cvs.openbsd.org>
2014-02-13 05:51:07 +0000

    if an attached sd(4) is readonly, make sure it's noticable in the dmesg, or write operations just fail with EACCES for no obvious reason
    ok krw@ tedu@

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sd.c,v 1.251 2013/11/01 17:36:19 krw Exp $	*/
+/*	$OpenBSD: sd.c,v 1.252 2014/02/13 05:51:06 halex Exp $	*/
 /*	$NetBSD: sd.c,v 1.111 1997/04/02 02:29:41 mycroft Exp $	*/
 
 /*-
@@ -229,6 +229,9 @@ sdattach(struct device *parent, struct device *self, void *aux)
 		    sc->sc_dev.dv_xname,
 		    dp->disksize / (1048576 / dp->secsize), dp->secsize,
 		    dp->disksize);
+		if (ISSET(sc_link->flags, SDEV_READONLY)) {
+			printf(", readonly");
+		}
 		printf("\n");
 		break;
 
