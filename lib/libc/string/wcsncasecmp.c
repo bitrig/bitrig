6e9bebf19df51eb0c60892ba56730785b1732eac
Patrick Wildt <patrick@blueri.se>
2014-03-02 08:38:04 +0100

    Use wcscasecmp and wcsncasecmp from FreeBSD.

--- lib/libc/string/wcsncasecmp.c
+++ lib/libc/string/wcsncasecmp.c
@@ -25,20 +25,25 @@
  */
 
 #include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 #include <wchar.h>
 #include <wctype.h>
 
 int
-wcscasecmp(const wchar_t *s1, const wchar_t *s2)
+wcsncasecmp(const wchar_t *s1, const wchar_t *s2, size_t n)
 {
 	wchar_t c1, c2;
 
+	if (n == 0)
+		return (0);
 	for (; *s1; s1++, s2++) {
 		c1 = towlower(*s1);
 		c2 = towlower(*s2);
 		if (c1 != c2)
 			return ((int)c1 - c2);
+		if (--n == 0)
+			return (0);
 	}
 	return (-*s2);
 }
