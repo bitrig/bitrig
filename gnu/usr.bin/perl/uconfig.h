4e5197de908caa56c98cf7d02d76697fc0dcf6b3
afresh1 <afresh1@cvs.openbsd.org>
2014-03-24 15:05:35 +0000

    Merge perl-5.18.2 plus local patches, remove old files
    OK espie@ sthen@ deraadt@

--- gnu/usr.bin/perl/uconfig.h
+++ gnu/usr.bin/perl/uconfig.h
@@ -912,6 +912,35 @@
 #define OSNAME "unknown"		/**/
 #define OSVERS "unknown"		/**/
 
+/* USE_CROSS_COMPILE:
+ *	This symbol, if defined, indicates that Perl is being cross-compiled.
+ */
+/* PERL_TARGETARCH:
+ *	This symbol, if defined, indicates the target architecture
+ *	Perl has been cross-compiled to.  Undefined if not a cross-compile.
+ */
+#ifndef USE_CROSS_COMPILE
+/*#define	USE_CROSS_COMPILE	/ **/
+#define	PERL_TARGETARCH	""	/**/
+#endif
+
+/* MULTIARCH:
+ *	This symbol, if defined, signifies that the build
+ *	process will produce some binary files that are going to be
+ *	used in a cross-platform environment.  This is the case for
+ *	example with the NeXT "fat" binaries that contain executables
+ *	for several CPUs.
+ */
+/*#define MULTIARCH		/ **/
+
+/* MEM_ALIGNBYTES:
+ *	This symbol contains the number of bytes required to align a
+ *	double, or a long double when applicable. Usual values are 2,
+ *	4 and 8. The default is eight, for safety.  For cross-compiling
+ *  	or multiarch support, Configure will set a minimum of 8.
+ */
+#define MEM_ALIGNBYTES 4
+
 /* ARCHLIB:
  *	This variable, if defined, holds the name of the directory in
  *	which the user wants to put architecture-dependent public
@@ -925,8 +954,8 @@
  *	This symbol contains the ~name expanded version of ARCHLIB, to be used
  *	in programs that are not prepared to deal with ~ expansion at run-time.
  */
-/*#define ARCHLIB "/usr/local/lib/perl5/5.16/unknown"		/ **/
-/*#define ARCHLIB_EXP "/usr/local/lib/perl5/5.16/unknown"		/ **/
+/*#define ARCHLIB "/usr/local/lib/perl5/5.18/unknown"		/ **/
+/*#define ARCHLIB_EXP "/usr/local/lib/perl5/5.18/unknown"		/ **/
 
 /* ARCHNAME:
  *	This symbol holds a string representing the architecture name.
@@ -950,7 +979,7 @@
  */
 #define BIN "/usr/local/bin"	/**/
 #define BIN_EXP "/usr/local/bin"	/**/
-/*#define PERL_RELOCATABLE_INC  		/ **/
+#define PERL_RELOCATABLE_INC "undef" 		/**/
 
 /* INTSIZE:
  *	This symbol contains the value of sizeof(int) so that the C
@@ -972,7 +1001,7 @@
  *	This symbol holds the hexadecimal constant defined in byteorder,
  *	in a UV, i.e. 0x1234 or 0x4321 or 0x12345678, etc...
  *	If the compiler supports cross-compiling or multiple-architecture
- *	binaries (eg. on NeXT systems), use compiler-defined macros to
+ *	binaries (e.g. on NeXT systems), use compiler-defined macros to
  *	determine the byte order.
  *	On NeXT 3.2 (and greater), you can build "Fat" Multiple Architecture
  *	Binaries (MAB) on either big endian or little endian machines.
@@ -2301,9 +2330,21 @@
  *	This symbol, if defined, indicates that the struct sockaddr_in6
  *	structure has a member called sin6_scope_id.
  */
+/* HAS_IP_MREQ:
+ *	This symbol, if defined, indicates the availability of
+ *	struct ip_mreq;
+ */
+/* HAS_IP_MREQ_SOURCE:
+ *	This symbol, if defined, indicates the availability of
+ *	struct ip_mreq_source;
+ */
 /* HAS_IPV6_MREQ:
  *	This symbol, if defined, indicates the availability of
  *	struct ipv6_mreq;
+ */
+/* HAS_IPV6_MREQ_SOURCE:
+ *	This symbol, if defined, indicates the availability of
+ *	struct ipv6_mreq_source;
  */
 /*#define	HAS_SOCKET		/ **/
 /*#define	HAS_SOCKETPAIR	/ **/
@@ -2316,7 +2357,10 @@
 /*#define	HAS_SCM_RIGHTS	/ **/
 /*#define	HAS_SOCKADDR_IN6	/ **/
 /*#define	HAS_SIN6_SCOPE_ID	/ **/
+/*#define	HAS_IP_MREQ	/ **/
+/*#define	HAS_IP_MREQ_SOURCE	/ **/
 /*#define	HAS_IPV6_MREQ	/ **/
+/*#define	HAS_IPV6_MREQ_SOURCE	/ **/
 
 /* HAS_SRAND48_R:
  *	This symbol, if defined, indicates that the srand48_r routine
@@ -2602,10 +2646,17 @@
 #define DOUBLESIZE 8		/**/
 
 /* EBCDIC:
- *     This symbol, if defined, indicates that this system uses
+ *	This symbol, if defined, indicates that this system uses
  *	EBCDIC encoding.
  */
+/* BOOTSTRAP_CHARSET:
+ *	This symbol, if defined, indicates that this system needs
+ *	converting various files to the native character set before
+ *	bringing up perl on a system that has a non-ASCII character
+ *	set and no working perl.
+ */
 /*#define	EBCDIC 		/ **/
+/*#define	BOOTSTRAP_CHARSET	/ **/
 
 /* Fpos_t:
  *	This symbol holds the type used to declare file positions in libc.
@@ -2620,7 +2671,7 @@
 #define	Gid_t_f		"lu"		/**/
 
 /* Gid_t_sign:
- *	This symbol holds the signedess of a Gid_t.
+ *	This symbol holds the signedness of a Gid_t.
  *	1 for unsigned, -1 for signed.
  */
 #define Gid_t_sign	1		/* GID sign */
@@ -2964,8 +3015,8 @@
  *	of the symbolic link pointing to the absolute pathname of
  *	the executing program.
  */
-#define PRIVLIB "/usr/local/lib/perl5/5.16"		/**/
-#define PRIVLIB_EXP "/usr/local/lib/perl5/5.16"		/**/
+#define PRIVLIB "/usr/local/lib/perl5/5.18"		/**/
+#define PRIVLIB_EXP "/usr/local/lib/perl5/5.18"		/**/
 
 /* CAN_PROTOTYPE:
  *	If defined, this macro indicates that the C compiler can handle
@@ -3108,8 +3159,8 @@
  *	system call is available to examine or change the signal mask
  *	of the calling process.
  */
-/*#define SITEARCH "/usr/local/lib/perl5/5.16/unknown"		/ **/
-/*#define SITEARCH_EXP "/usr/local/lib/perl5/5.16/unknown"		/ **/
+/*#define SITEARCH "/usr/local/lib/perl5/5.18/unknown"		/ **/
+/*#define SITEARCH_EXP "/usr/local/lib/perl5/5.18/unknown"		/ **/
 
 /* USE_SITECUSTOMIZE:
  *	This symbol, if defined, indicates that sitecustomize should
@@ -3127,8 +3178,8 @@
  *	This symbol, if defined, indicates that the vsnprintf () library
  *	function is available for use.
  */
-#define SITELIB "/usr/local/lib/perl5/5.16"		/**/
-#define SITELIB_EXP "/usr/local/lib/perl5/5.16"		/**/
+#define SITELIB "/usr/local/lib/perl5/5.18"		/**/
+#define SITELIB_EXP "/usr/local/lib/perl5/5.18"		/**/
 #define SITELIB_STEM "/usr/local/lib/perl5"		/**/
 
 /* HAS_SOCKATMARK:
@@ -3170,9 +3221,9 @@
  */
 /*#define HAS_STRUCT_STATFS	/ **/
 
-/* HAS_FSTATVFS:
- *	This symbol, if defined, indicates that the fstatvfs routine is
- *	available to stat filesystems by file descriptors.
+/* Uid_t_sign:
+ *	This symbol holds the signedness of a Uid_t.
+ *	1 for unsigned, -1 for signed.
  */
 /*#define HAS_FSTATVFS		/ **/
 
@@ -3649,7 +3700,7 @@
 /*#define     HAS_INT64_T               / **/
 
 /* HAS_ISBLANK:
- *	This manifest constant lets the C program know that isblank 
+ *	This manifest constant lets the C program know that isblank
  *	is available.
  */
 /*#define HAS_ISBLANK		/ **/
@@ -4529,7 +4580,7 @@
  *	This variable contains the size of struct stat's st_ino in bytes.
  */
 /* ST_INO_SIGN:
- *	This symbol holds the signedess of struct stat's st_ino.
+ *	This symbol holds the signedness of struct stat's st_ino.
  *	1 for unsigned, -1 for signed.
  */
 #define ST_INO_SIGN 1	/* st_ino sign */
@@ -4682,6 +4733,6 @@
 #endif
 
 /* Generated from:
- * d9b6a68b192c72c0695b560fae6e4916b381f55df5fdf7911b0ef434840f092e config_h.SH
- * 2a7f58d271710106d7cb7ac1d0ea33a1caefa9ad74033041bb9996626786941b uconfig.sh
+ * 2c9dc3f21d37b1665f6a59dfc6d79e6cb08bdf36a9c3e427d11d6b9ddffe2439 config_h.SH
+ * 26ab9b4aa382d32761cb91084ba59e7e4b190799502a43366ccb3d2f584783ca uconfig.sh
  * ex: set ro: */
