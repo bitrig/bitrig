094a7d31432316e999b05dcb944077ec1cc07cae
Brad Smith <brad@cvs.openbsd.org>
2014-07-12 20:07:35 +0000

    Mark the interface down and cancel the watchdog timer in imxenet_stop().
    ok matthieu@ rapha@

--- sys/arch/armv7/imx/imxenet.c
+++ sys/arch/armv7/imx/imxenet.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: imxenet.c,v 1.5 2014/07/02 17:01:07 brad Exp $ */
+/* $OpenBSD: imxenet.c,v 1.6 2014/07/12 20:07:34 brad Exp $ */
 /*
  * Copyright (c) 2012-2013 Patrick Wildt <patrick@blueri.se>
  *
@@ -624,7 +624,9 @@ imxenet_stop(struct imxenet_softc *sc)
 {
 	struct ifnet *ifp = &sc->sc_ac.ac_if;
 
-	/* Tell the stack that the interface is no longer active */
+	/*
+	 * Mark the interface down and cancel the watchdog timer.
+	 */
 	ifp->if_flags &= ~(IFF_RUNNING | IFF_OACTIVE);
 	ifp->if_timer = 0;
 
