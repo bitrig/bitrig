36190980c25afa37ea268b1de9256a7c4450e4cf
Miod Vallat <miod@cvs.openbsd.org>
2014-08-30 10:41:11 +0000

    Make sure we check for GPT in writedisklabel() if kernel is compiled with GPT support, for it would risk being overwritten otherwise.
    From Markus Mueller

--- sys/arch/amd64/amd64/disksubr.c
+++ sys/arch/amd64/amd64/disksubr.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disksubr.c,v 1.67 2014/07/13 15:32:28 miod Exp $	*/
+/*	$OpenBSD: disksubr.c,v 1.68 2014/08/30 10:41:10 miod Exp $	*/
 /*	$NetBSD: disksubr.c,v 1.21 1996/05/03 19:42:03 christos Exp $	*/
 
 /*
