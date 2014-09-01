62d5e65d00c2551927b281f4212093e8fc5355f9
Philip Guenther <guenther@cvs.openbsd.org>
2014-09-01 03:37:11 +0000

    Add explicit membars, so that we can stop requiring rw_cas() to provide the magic.
    ok matthew@ dlg@

--- sys/kern/kern_rwlock.c
+++ sys/kern/kern_rwlock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_rwlock.c,v 1.22 2014/07/09 13:32:00 guenther Exp $	*/
+/*	$OpenBSD: kern_rwlock.c,v 1.23 2014/09/01 03:37:10 guenther Exp $	*/
 
 /*
  * Copyright (c) 2002, 2003 Artur Grabowski <art@openbsd.org>
@@ -22,6 +22,7 @@
 #include <sys/proc.h>
 #include <sys/rwlock.h>
 #include <sys/limits.h>
+#include <sys/atomic.h>
 
 #include <machine/lock.h>
 
@@ -87,6 +88,8 @@ rw_enter_read(struct rwlock *rwl)
 	if (__predict_false((owner & RWLOCK_WRLOCK) ||
 	    rw_cas(&rwl->rwl_owner, owner, owner + RWLOCK_READ_INCR)))
 		rw_enter(rwl, RW_READ);
+	else
+		membar_enter();
 }
 
 void
@@ -99,6 +102,8 @@ rw_enter_write(struct rwlock *rwl)
 	if (__predict_false(rw_cas(&rwl->rwl_owner, 0,
 	    RW_PROC(p) | RWLOCK_WRLOCK)))
 		rw_enter(rwl, RW_WRITE);
+	else
+		membar_enter();
 }
 
 void
@@ -108,6 +113,7 @@ rw_exit_read(struct rwlock *rwl)
 
 	rw_assert_rdlock(rwl);
 
+	membar_exit();
 	if (__predict_false((owner & RWLOCK_WAIT) ||
 	    rw_cas(&rwl->rwl_owner, owner, owner - RWLOCK_READ_INCR)))
 		rw_exit(rwl);
@@ -120,6 +126,7 @@ rw_exit_write(struct rwlock *rwl)
 
 	rw_assert_wrlock(rwl);
 
+	membar_exit();
 	if (__predict_false((owner & RWLOCK_WAIT) ||
 	    rw_cas(&rwl->rwl_owner, owner, 0)))
 		rw_exit(rwl);
@@ -222,6 +229,7 @@ retry:
 
 	if (__predict_false(rw_cas(&rwl->rwl_owner, o, o + inc)))
 		goto retry;
+	membar_enter();
 
 	/*
 	 * If old lock had RWLOCK_WAIT and RWLOCK_WRLOCK set, it means we
@@ -247,6 +255,7 @@ rw_exit(struct rwlock *rwl)
 	else
 		rw_assert_rdlock(rwl);
 
+	membar_exit();
 	do {
 		owner = rwl->rwl_owner;
 		if (wrlock)
