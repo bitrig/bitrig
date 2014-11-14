2299b501666bbfb39e3d636decc9a062e4bb5d3f
Ted Unangst <tedu@cvs.openbsd.org>
2014-11-14 23:26:49 +0000

    prefer sizeof(*ptr) to sizeof(struct) for malloc and free

--- sys/kern/exec_elf.c
+++ sys/kern/exec_elf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: exec_elf.c,v 1.104 2014/11/06 17:31:19 tedu Exp $	*/
+/*	$OpenBSD: exec_elf.c,v 1.105 2014/11/14 23:26:48 tedu Exp $	*/
 
 /*
  * Copyright (c) 1996 Per Fogelstrom
@@ -743,7 +743,7 @@ ELFNAME2(exec,makecmds)(struct proc *p, struct exec_package *epp)
 	 * If the binary is dynamically linked set the interp appropriately.
 	 */
 
-	ap = malloc(sizeof(struct elf_args), M_TEMP, M_WAITOK);
+	ap = malloc(sizeof(*ap), M_TEMP, M_WAITOK);
 
 	ap->arg_phaddr = phdr;
 	ap->arg_phentsize = eh->e_phentsize;
