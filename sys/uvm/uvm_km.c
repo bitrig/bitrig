1eb4c3d4bc410bd5133cf2737f50a63813b05745
Theo de Raadt <deraadt@cvs.openbsd.org>
2014-11-17 04:26:54 +0000

    There is no reason for uvm_km_alloc1() to allocate kernel memory that is executable. ok tedu kettenis guenther

--- sys/uvm/uvm_km.c
+++ sys/uvm/uvm_km.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_km.c,v 1.117 2014/11/16 12:31:00 deraadt Exp $	*/
+/*	$OpenBSD: uvm_km.c,v 1.118 2014/11/17 04:26:53 deraadt Exp $	*/
 /*	$NetBSD: uvm_km.c,v 1.42 2001/01/14 02:10:01 thorpej Exp $	*/
 
 /* 
