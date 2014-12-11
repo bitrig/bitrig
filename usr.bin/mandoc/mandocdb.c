2db3dd3e9da9bb2f60ebb59ed63055f3923f6b42
Martin Natano <natano@natano.net>
2014-12-11 20:51:33 +0100

    Revert "We do not have _tolower, so call tolower instead."
    This reverts commit af4cdda3d8e0832e929f8098a62c3d37eb586b17.
    
    We do have _tolower, it was just missing from the ctype.h header. Sorry
    for the inconvenience!

--- usr.bin/mandoc/mandocdb.c
+++ usr.bin/mandoc/mandocdb.c
@@ -1433,7 +1433,7 @@ putkey(const struct mpage *mpage, char *value, uint64_t type)
 	if (TYPE_arch == type)
 		for (cp = value; *cp; cp++)
 			if (isupper((unsigned char)*cp))
-				*cp = tolower((unsigned char)*cp);
+				*cp = _tolower((unsigned char)*cp);
 	putkeys(mpage, value, strlen(value), type);
 }
 
