e7708f0f5f16257bee74c66caf7066c5d677ff81
Theo de Raadt <deraadt@cvs.openbsd.org>
2015-01-14 21:17:53 +0000

    This does not need <sys/param.h>.  If we change some other stuff around, it might not need <sys/types.h> either.  Even further changes could eliminate it.  Anyone up for the exercise?

--- sys/sys/localedef.h
+++ sys/sys/localedef.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: localedef.h,v 1.3 1996/04/21 22:31:47 deraadt Exp $	*/
+/*	$OpenBSD: localedef.h,v 1.5 2015/01/14 21:17:52 deraadt Exp $	*/
 /*	$NetBSD: localedef.h,v 1.4 1996/04/09 20:55:31 cgd Exp $	*/
 
 /*
@@ -34,7 +34,6 @@
 #ifndef _SYS_LOCALEDEF_H_
 #define _SYS_LOCALEDEF_H_
 
-#include <sys/param.h>
 #include <sys/types.h>
 
 typedef struct
