016ab1e2086b9d1f2d800fc0dc63029c4fa6fd25
Theo de Raadt <deraadt@cvs.openbsd.org>
2015-01-20 17:37:55 +0000

    use <limits.h> comprehensively.  For now try to push <> includes to each .c file, and out of the .h files.  To avoid overinclude. ok gilles, in principle.  If this has been done right, -portable should become easier to maintain.

--- usr.sbin/smtpd/esc.c
+++ usr.sbin/smtpd/esc.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: esc.c,v 1.2 2014/04/19 17:32:58 gilles Exp $	 */
+/* $OpenBSD: esc.c,v 1.3 2015/01/20 17:37:54 deraadt Exp $	 */
 
 /*
  * Copyright (c) 2014 Gilles Chehade <gilles@poolp.org>
@@ -17,7 +17,14 @@
  */
 
 #include <sys/param.h>
+#include <sys/queue.h>
+#include <sys/tree.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <netdb.h>
 #include <stdio.h>
+#include <limits.h>
 
 #include "smtpd-defines.h"
 #include "smtpd-api.h"
