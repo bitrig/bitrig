bitrig/bitrig

commit 96feb3aa4bb45cc45d25b3fd2d8c451060926339

fix race condition in bwrite()
in bwrite(), don't inspect b_bq after calling VOP_STRATEGY() and
lowering the IPL. at that point we no longer own the buffer, so it is
possible for the issued i/o to complete between the moment we check if
b_bq is set and the subsequent call to bufq_wait(), leading to a null
pointer dereference in the latter function, as observed empirically this
morning on my amd64 running dpb -p 8.

ok patrick@ dhill@

sys/kern/kern_bufq.c
@@ -226,7 +226,7 @@ bufq_drain(struct bufq *bq)
 }
 
 void
-bufq_wait(struct bufq *bq, struct buf *bp)
+bufq_wait(struct bufq *bq)
 {
 	if (bq->bufq_hi) {
 		assertwaitok();

sys/kern/vfs_bio.c
@@ -558,6 +558,7 @@ bwrite(struct buf *bp)
 	int rv, async, wasdelayed, s;
 	struct vnode *vp;
 	struct mount *mp;
+	struct bufq *bq;
 
 	vp = bp->b_vp;
 	if (vp != NULL)
@@ -618,6 +619,7 @@ bwrite(struct buf *bp)
 
 	/* Initiate disk write.  Make sure the appropriate party is charged. */
 	bp->b_vp->v_numoutput++;
+	bq = bp->b_bq;
 	splx(s);
 	SET(bp->b_flags, B_WRITEINPROG);
 	VOP_STRATEGY(bp);
@@ -627,8 +629,8 @@ bwrite(struct buf *bp)
 	 * the number of outstanding write bufs drops below the low
 	 * water mark.
 	 */
-	if (bp->b_bq)
-		bufq_wait(bp->b_bq, bp);
+	if (bq)
+		bufq_wait(bq);
 
 	if (async)
 		return (0);

sys/sys/buf.h
@@ -99,7 +99,7 @@ void		 bufq_requeue(struct bufq *, struct buf *);
 int		 bufq_peek(struct bufq *);
 void		 bufq_drain(struct bufq *);
 
-void		 bufq_wait(struct bufq *, struct buf *);
+void		 bufq_wait(struct bufq *);
 void		 bufq_done(struct bufq *, struct buf *);
 void		 bufq_quiesce(void);
 void		 bufq_restart(void);

commit d6b7ee487cd9f1342ddc5e83e8dba767b4a0ca1d

call bufq_wait() from VOP_STRATEGY()
have drivers using bufq, i.e calling bufq_queue() from their strategy
routines, call bufq_wait() from the same function. this fixes 96feb3aa,
which mistakenly assumed b_bq to have been set prior to the VOP_STRATEGY
call in bwrite().

there are two differences in behaviour introduced by this change:

- we no longer bufq_wait() on synchronous writes;
- we also bufq_wait() on asynchronous writes that originate in places
  other than bwrite() (i.e, other VOP_STRATEGY() callers);

ok patrick@

sys/dev/ata/wd.c
@@ -401,7 +401,7 @@ void
 wdstrategy(struct buf *bp)
 {
 	struct wd_softc *wd;
-	int s;
+	int s, bqwait;
 
 	wd = wdlookup(DISKUNIT(bp->b_dev));
 	if (wd == NULL) {
@@ -429,10 +429,12 @@ wdstrategy(struct buf *bp)
 	}
 
 	/* Queue transfer on drive, activate drive and controller if idle. */
-	bufq_queue(&wd->sc_bufq, bp);
+	bufq_queue(&wd->sc_bufq, bp, &bqwait);
 	s = splbio();
 	wdstart(wd);
 	splx(s);
+	if (bqwait)
+		bufq_wait(&wd->sc_bufq);
 	device_unref(&wd->sc_dev);
 	return;
 

sys/kern/kern_bufq.c
@@ -165,7 +165,7 @@ bufq_destroy(struct bufq *bq)
 
 
 void
-bufq_queue(struct bufq *bq, struct buf *bp)
+bufq_queue(struct bufq *bq, struct buf *bp, int *bqwait)
 {
 	mtx_enter(&bq->bufq_mtx);
 	while (bq->bufq_stop) {
@@ -176,6 +176,9 @@ bufq_queue(struct bufq *bq, struct buf *bp)
 	bq->bufq_outstanding++;
 	bq->bufq_impl->impl_queue(bq->bufq_data, bp);
 	mtx_leave(&bq->bufq_mtx);
+
+	/* If it's an asynchronous write, the caller should bufq_wait(). */
+	*bqwait = (bp->b_flags & B_READ) == 0 && (bp->b_flags & B_ASYNC);
 }
 
 struct buf *

sys/kern/vfs_bio.c
@@ -558,7 +558,6 @@ bwrite(struct buf *bp)
 	int rv, async, wasdelayed, s;
 	struct vnode *vp;
 	struct mount *mp;
-	struct bufq *bq;
 
 	vp = bp->b_vp;
 	if (vp != NULL)
@@ -619,31 +618,22 @@ bwrite(struct buf *bp)
 
 	/* Initiate disk write.  Make sure the appropriate party is charged. */
 	bp->b_vp->v_numoutput++;
-	bq = bp->b_bq;
 	splx(s);
 	SET(bp->b_flags, B_WRITEINPROG);
 	VOP_STRATEGY(bp);
 
-	/*
-	 * If the queue is above the high water mark, wait till
-	 * the number of outstanding write bufs drops below the low
-	 * water mark.
-	 */
-	if (bq)
-		bufq_wait(bq);
-
-	if (async)
-		return (0);
+	if (async == 0) {
+		rv = biowait(bp);	/* Wait for completion. */
+		brelse(bp);
+		return (rv);
+	}
 
 	/*
-	 * If I/O was synchronous, wait for it to complete.
+	 * In the asynchronous case, we no longer own the buffer. It is under
+	 * control of the device driver until biodone() happens.
 	 */
-	rv = biowait(bp);
 
-	/* Release the buffer. */
-	brelse(bp);
-
-	return (rv);
+	return (0);
 }
 
 /*

sys/scsi/cd.c
@@ -444,7 +444,7 @@ void
 cdstrategy(struct buf *bp)
 {
 	struct cd_softc *sc;
-	int s;
+	int s, bqwait;
 
 	sc = cdlookup(DISKUNIT(bp->b_dev));
 	if (sc == NULL) {
@@ -472,7 +472,7 @@ cdstrategy(struct buf *bp)
 		goto done;
 
 	/* Place it in the queue of disk activities for this disk. */
-	bufq_queue(&sc->sc_bufq, bp);	
+	bufq_queue(&sc->sc_bufq, bp, &bqwait);
 
 	/*
 	 * Tell the device to get going on the transfer if it's
@@ -480,6 +480,8 @@ cdstrategy(struct buf *bp)
 	 */
 	scsi_xsh_add(&sc->sc_xsh);
 
+	if (bqwait)
+		bufq_wait(&sc->sc_bufq);
 	device_unref(&sc->sc_dev);
 	return;
 

sys/scsi/sd.c
@@ -501,7 +501,7 @@ void
 sdstrategy(struct buf *bp)
 {
 	struct sd_softc *sc;
-	int s;
+	int s, bqwait;
 
 	sc = sdlookup(DISKUNIT(bp->b_dev));
 	if (sc == NULL) {
@@ -531,7 +531,7 @@ sdstrategy(struct buf *bp)
 		goto done;
 
 	/* Place it in the queue of disk activities for this disk. */
-	bufq_queue(&sc->sc_bufq, bp);
+	bufq_queue(&sc->sc_bufq, bp, &bqwait);
 
 	/*
 	 * Tell the device to get going on the transfer if it's
@@ -539,6 +539,8 @@ sdstrategy(struct buf *bp)
 	 */
 	scsi_xsh_add(&sc->sc_xsh);
 
+	if (bqwait)
+		bufq_wait(&sc->sc_bufq);
 	device_unref(&sc->sc_dev);
 	return;
 

sys/scsi/st.c
@@ -823,7 +823,7 @@ ststrategy(struct buf *bp)
 {
 	struct scsi_link *sc_link;
 	struct st_softc *st;
-	int s;
+	int s, bqwait;
 
 	st = stlookup(STUNIT(bp->b_dev));
 	if (st == NULL) {
@@ -872,7 +872,7 @@ ststrategy(struct buf *bp)
 	 * at the end (a bit silly because we only have on user..
 	 * (but it could fork()))
 	 */
-	bufq_queue(&st->sc_bufq, bp);
+	bufq_queue(&st->sc_bufq, bp, &bqwait);
 
 	/*
 	 * Tell the device to get going on the transfer if it's
@@ -881,6 +881,8 @@ ststrategy(struct buf *bp)
 	 */
 	scsi_xsh_add(&st->sc_xsh);
 
+	if (bqwait)
+		bufq_wait(&st->sc_bufq);
 	device_unref(&st->sc_dev);
 	return;
 bad:

sys/sys/buf.h
@@ -93,7 +93,7 @@ int		 bufq_init(struct bufq *, int);
 int		 bufq_switch(struct bufq *, int);
 void		 bufq_destroy(struct bufq *);
 
-void		 bufq_queue(struct bufq *, struct buf *);
+void		 bufq_queue(struct bufq *, struct buf *, int *);
 struct buf	*bufq_dequeue(struct bufq *);
 void		 bufq_requeue(struct bufq *, struct buf *);
 int		 bufq_peek(struct bufq *);

sys/uvm/uvm_swap.c
@@ -1156,7 +1156,7 @@ sw_reg_strategy(struct swapdev *sdp, struct buf *bp, int bn)
 	daddr_t	nbn;
 	caddr_t		addr;
 	off_t		byteoff;
-	int		s, off, nra, error, sz, resid;
+	int		s, off, nra, error, sz, resid, bqwait;
 
 	/*
 	 * allocate a vndxfer head for this transfer and point it to
@@ -1286,7 +1286,7 @@ sw_reg_strategy(struct swapdev *sdp, struct buf *bp, int bn)
 		bgetvp(vp, &nbp->vb_buf);
 
 		/* start I/O if we are not over our limit */
-		bufq_queue(&sdp->swd_bufq, &nbp->vb_buf);
+		bufq_queue(&sdp->swd_bufq, &nbp->vb_buf, &bqwait);
 		sw_reg_start(sdp);
 		splx(s);
 
@@ -1310,6 +1310,8 @@ out: /* Arrive here at splbio */
 		biodone(bp);
 	}
 	splx(s);
+	if (bqwait)
+		bufq_wait(&sdp->swd_bufq);
 }
 
 /* sw_reg_start: start an I/O request on the requested swapdev. */