f63cf3398c65fc557e4ed55ce6fe3523c5f26650
Mike Larkin <mlarkin@cvs.openbsd.org>
2015-01-28 02:56:51 +0000

    Remove an unused macro and a stale comment. No functional change.

--- sys/arch/amd64/amd64/pmap.c
+++ sys/arch/amd64/amd64/pmap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pmap.c,v 1.85 2015/01/15 15:30:17 sf Exp $	*/
+/*	$OpenBSD: pmap.c,v 1.86 2015/01/28 02:56:50 mlarkin Exp $	*/
 /*	$NetBSD: pmap.c,v 1.3 2003/05/08 18:13:13 thorpej Exp $	*/
 
 /*
@@ -205,8 +205,6 @@ long nkptp[] = NKPTP_INITIALIZER;
 long nkptpmax[] = NKPTPMAX_INITIALIZER;
 long nbpd[] = NBPD_INITIALIZER;
 pd_entry_t *normal_pdes[] = PDES_INITIALIZER;
-
-/* int nkpde = NKPTP; */
 
 /*
  * This is a bit of a hack to ensure that UVMLOCKDEBUG asserts in
