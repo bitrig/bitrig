0ea8aa0485f0b489ab3e8007196610e74d6a510d
Patrick Wildt <patrick@blueri.se>
2015-02-09 18:56:51 +0100

    arm: sort fdtbus functions, add comments

--- sys/arch/arm/fdt/fdtbus.c
+++ sys/arch/arm/fdt/fdtbus.c
@@ -47,6 +47,17 @@ struct arm32_bus_dma_tag fdt_bus_dma_tag = {
 	_bus_dmamem_mmap,
 };
 
+static int fdt_match(struct device *, void *, void *);
+static void fdt_attach(struct device *, struct device *, void *);
+static void fdt_find_match(struct device *, void *);
+static void fdt_iterate(struct device *, struct device *, void *);
+static void fdt_try_node(struct device *, void *, void *);
+
+/*
+ * We need to look through the tree at least twice.  To make sure we
+ * do not attach node multiple times, keep a list of nodes that are
+ * already handled by any kind of driver.
+ */
 SLIST_HEAD(, fdt_entry) fdt_dev_list = SLIST_HEAD_INITIALIZER(fdt_dev_list);
 struct fdt_entry {
 	SLIST_ENTRY(fdt_entry)	 fe_list;
@@ -55,12 +66,85 @@ struct fdt_entry {
 };
 
 static int
+fdt_is_attached(void *node)
+{
+	struct fdt_entry *fe;
+
+	SLIST_FOREACH(fe, &fdt_dev_list, fe_list)
+		if (fe->fe_node == node)
+			return (1);
+
+	return (0);
+}
+
+struct cfattach fdt_ca = {
+	sizeof(struct armv7_softc), fdt_match, fdt_attach, NULL,
+	config_activate_children
+};
+
+struct cfdriver fdt_cd = {
+	NULL, "fdt", DV_DULL
+};
+
+/*
+ * FDT bus always attaches when there is some kind of FDT available.
+ */
+static int
 fdt_match(struct device *parent, void *cfdata, void *aux)
 {
 	if (fdt_next_node(0) != NULL)
 		return (1);
 
 	return (0);
+}
+
+static void
+fdt_attach(struct device *parent, struct device *self, void *aux)
+{
+	char *compatible;
+	void *node = fdt_next_node(0);
+	if (node == NULL) {
+		printf(": tree not found\n");
+		return;
+	}
+
+	if (fdt_node_property(node, "compatible", &compatible))
+		printf(": %s compatible\n", compatible);
+	else
+		printf(": unknown\n");
+
+	config_scan(fdt_find_match, self);
+}
+
+/*
+ * Usually you should be able to attach devices in the order
+ * specified in the device tree.  Due to how a few drivers
+ * are written we need to make sure we attach them in the
+ * order stated in files.xxx.
+ *
+ * This means for every driver enables, we go through the
+ * FDT and look for compatible nodes.
+ */
+static void
+fdt_find_match(struct device *self, void *match)
+{
+	fdt_iterate(self, match, fdt_next_node(0));
+}
+
+static void
+fdt_iterate(struct device *self, struct device *match, void *node)
+{
+	for (;
+	    node != NULL;
+	    node = fdt_next_node(node))
+	{
+		/* skip nodes that are already handled by some driver */
+		if (fdt_is_attached(node))
+			continue;
+
+		fdt_iterate(self, match, fdt_child_node(node));
+		fdt_try_node(self, match, node);
+	}
 }
 
 static void
@@ -97,64 +181,3 @@ fdt_try_node(struct device *self, void *match, void *node)
 		SLIST_INSERT_HEAD(&fdt_dev_list, fe, fe_list);
 	}
 }
-
-static int
-fdt_is_attached(void *node)
-{
-	struct fdt_entry *fe;
-
-	SLIST_FOREACH(fe, &fdt_dev_list, fe_list)
-		if (fe->fe_node == node)
-			return (1);
-
-	return (0);
-}
-
-static void
-fdt_iterate(struct device *self, struct device *match, void *node)
-{
-	for (;
-	    node != NULL;
-	    node = fdt_next_node(node))
-	{
-		/* skip nodes that are already handled by some driver */
-		if (fdt_is_attached(node))
-			continue;
-
-		fdt_iterate(self, match, fdt_child_node(node));
-		fdt_try_node(self, match, node);
-	}
-}
-
-static void
-fdt_find_match(struct device *self, void *match)
-{
-	fdt_iterate(self, match, fdt_next_node(0));
-}
-
-static void
-fdt_attach(struct device *parent, struct device *self, void *aux)
-{
-	char *compatible;
-	void *node = fdt_next_node(0);
-	if (node == NULL) {
-		printf(": tree not found\n");
-		return;
-	}
-
-	if (fdt_node_property(node, "compatible", &compatible))
-		printf(": %s compatible\n", compatible);
-	else
-		printf(": unknown\n");
-
-	config_scan(fdt_find_match, self);
-}
-
-struct cfattach fdt_ca = {
-	sizeof(struct armv7_softc), fdt_match, fdt_attach, NULL,
-	config_activate_children
-};
-
-struct cfdriver fdt_cd = {
-	NULL, "fdt", DV_DULL
-};
