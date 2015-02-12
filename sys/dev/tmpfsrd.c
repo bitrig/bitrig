8d08a146ffb002e4b549491a3fdf55e9fe3cdf1c
Pedro Martelletto <pedro@ambientworks.net>
2015-02-12 16:37:01 +0100

    Assign copyright to genua mbh. Discussed with Markus.

--- sys/dev/tmpfsrd.c
+++ sys/dev/tmpfsrd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Pedro Martelletto
+ * Copyright (c) 2014, 2015 genua mbh. All rights reserved.
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -12,6 +12,8 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Written by Pedro Martelletto <pedro@ambientworks.net>.
  */
 
 #include <sys/param.h>
