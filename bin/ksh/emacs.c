bitrig/bitrig

commit 67223da12270b6a95cf1189e400a14b958593246

Check for SIGWINCH immediately before waiting for user input.
There is still room for race conditions, where a signal is delivered
before the global variable has been checked and the next read syscall
is started, but it should get less likely to happen.

ok marco@

bin/ksh/emacs.c
@@ -1764,7 +1764,7 @@ x_e_getc(int redr)
 		macro_args = NULL;
 	}
 
-	while ((c = x_getc()) < 0 && errno == EINTR) {
+	while (got_sigwinch || (c = x_getc()) < 0 && errno == EINTR) {
 		if (redr) {
 			x_redraw(1);
 			x_flush();

commit 39471e3cd825f68e612417ebcfb974f94cf166fc

Fix x_e_getc() to not enter an endless loop.
The read loop in x_e_getc() failed to clear the got_sigwinch variable
and wound up in an endless loop. The bug (that has been introduced in
67223da12) can be triggered in various ways:

 * press ctrl-r, resize window
 * bind the version command to a keybinding and press it, resize window
 * press alt-1, resize window

The code in question was a special case for x_e_getc() that doesn't
redraw the prompt, when SIGWINCH arrives.  After careful inspection of
all the callers (x_e_getc(0)), I conclude that, in fact, all the callers
that used the non-redrawing variant did so in cases where redrawing
would have been beneficial. The diff at hand removes the 'redr'
parameter from x_e_getc(), such that that function always redraws the
prompt when SIGWINCH arrives, circumventing the bug described above.

reported by and ok pedro@

bin/ksh/emacs.c
@@ -123,7 +123,7 @@ static int	x_match(char *, char *);
 static void	x_redraw(int);
 static void	x_push(int);
 static void	x_e_ungetc(int);
-static int	x_e_getc(int);
+static int	x_e_getc(void);
 static void	x_e_putc(int);
 static void	x_e_puts(const char *);
 static int	x_comment(int);
@@ -282,7 +282,7 @@ x_emacs(char *buf, size_t len)
 	x_last_command = NULL;
 	while (1) {
 		x_flush();
-		if ((c = x_e_getc(1)) < 0)
+		if ((c = x_e_getc()) < 0)
 			return 0;
 
 		line[at++] = c;
@@ -690,7 +690,7 @@ x_search_char_forw(int c)
 	char *cp = xcp;
 
 	*xep = '\0';
-	c = x_e_getc(1);
+	c = x_e_getc();
 	while (x_arg--) {
 		if (c < 0 ||
 		    ((cp = (cp == xep) ? NULL : strchr(cp + 1, c)) == NULL &&
@@ -708,7 +708,7 @@ x_search_char_back(int c)
 {
 	char *cp = xcp, *p;
 
-	c = x_e_getc(1);
+	c = x_e_getc();
 	for (; x_arg--; cp = p)
 		for (p = cp; ; ) {
 			if (p-- == xbuf)
@@ -823,7 +823,7 @@ x_search_hist(int c)
 	x_redraw(1);
 	while (1) {
 		x_flush();
-		if ((c = x_e_getc(offset >= 0)) < 0)
+		if ((c = x_e_getc()) < 0)
 			return (KSTD);
 		f = kb_find_hist_func(c);
 		if (c == CTRL('[')) {
@@ -1586,7 +1586,7 @@ x_version(int c)
 	x_redraw(1);
 	x_flush();
 
-	c = x_e_getc(0);
+	c = x_e_getc();
 	xbuf = o_xbuf;
 	xend = o_xend;
 	xep = o_xep;
@@ -1749,7 +1749,7 @@ x_e_ungetc(int c)
 }
 
 static int
-x_e_getc(int redr)
+x_e_getc(void)
 {
 	int c;
 
@@ -1765,10 +1765,8 @@ x_e_getc(int redr)
 	}
 
 	while (got_sigwinch || ((c = x_getc()) < 0 && errno == EINTR)) {
-		if (redr) {
-			x_redraw(1);
-			x_flush();
-		}
+		x_redraw(1);
+		x_flush();
 	}
 
 	return (c);
@@ -1831,7 +1829,7 @@ x_set_arg(int c)
 	int n = 0;
 	int first = 1;
 
-	for (; c >= 0 && isdigit(c); c = x_e_getc(0), first = 0)
+	for (; c >= 0 && isdigit(c); c = x_e_getc(), first = 0)
 		n = n * 10 + (c - '0');
 	if (c < 0 || first) {
 		x_e_putc('\a');