bitrig/bitrig

commit 747733d825a61fb587a29971b42f21eb2ea3d6cb

Change tmpfs_update() to take two arguments: a tmpfs_node_t and an optional struct timespec pointer. Don't update timestamps of read-only nodes.
OK natano@.

sys/tmpfs/tmpfs_subr.c
@@ -514,7 +514,7 @@ tmpfs_dir_attach(tmpfs_node_t *dnode, tmpfs_dirent_t *de, tmpfs_node_t *node)
 	TAILQ_INSERT_TAIL(&dnode->tn_spec.tn_dir.tn_dir, de, td_entries);
 	dnode->tn_size += sizeof(tmpfs_dirent_t);
 	dnode->tn_status |= TMPFS_NODE_MODIFIED | TMPFS_NODE_CHANGED;
-	tmpfs_update(dvp, NULL, NULL, 0);
+	tmpfs_update(dnode, NULL);
 	uvm_vnp_setsize(dvp, dnode->tn_size);
 
 	if (node->tn_type == VDIR) {
@@ -584,7 +584,7 @@ tmpfs_dir_detach(tmpfs_node_t *dnode, tmpfs_dirent_t *de)
 	dnode->tn_status |= TMPFS_NODE_MODIFIED | TMPFS_NODE_CHANGED;
 	tmpfs_dir_putseq(dnode, de);
 	if (dvp) {
-		tmpfs_update(dvp, NULL, NULL, 0);
+		tmpfs_update(dnode, NULL);
 		uvm_vnp_setsize(dvp, dnode->tn_size);
 		VN_KNOTE(dvp, events);
 	}
@@ -1143,16 +1143,15 @@ tmpfs_chtimes(struct vnode *vp, const struct timespec *atime,
 	if (cred->cr_uid != node->tn_uid && (error = suser_ucred(cred)) &&
 	    ((vaflags & VA_UTIMES_NULL) == 0 ||
 	    (error = VOP_ACCESS(vp, VWRITE, cred))))
-	    	return error;
+		return error;
 
 	if (atime->tv_nsec != UTIME_OMIT)
-		node->tn_status |= TMPFS_NODE_ACCESSED;
-
+		node->tn_atime = *atime;
 	if (mtime->tv_nsec != UTIME_OMIT)
-		node->tn_status |= TMPFS_NODE_MODIFIED;
+		node->tn_mtime = *mtime;
 
-	tmpfs_update(vp, atime, mtime, 0);
 	VN_KNOTE(vp, NOTE_ATTRIB);
+
 	return 0;
 }
 
@@ -1160,32 +1159,26 @@ tmpfs_chtimes(struct vnode *vp, const struct timespec *atime,
  * tmpfs_update: update timestamps, et al.
  */
 void
-tmpfs_update(struct vnode *vp, const struct timespec *acc,
-    const struct timespec *mod, int flags)
+tmpfs_update(tmpfs_node_t *node, const struct timespec *tm)
 {
-	tmpfs_node_t *node = VP_TO_TMPFS_NODE(vp);
 	struct timespec nowtm;
 
-	/* KASSERT(VOP_ISLOCKED(vp)); */
-#if 0
-	if (flags & UPDATE_CLOSE) {
-		/* XXX Need to do anything special? */
-	}
-#endif
-	if ((node->tn_status & TMPFS_NODE_STATUSALL) == 0) {
+	if (node->tn_status & TMPFS_NODE_READONLY) {
+		node->tn_status &= ~TMPFS_NODE_STATUSALL;
 		return;
 	}
-	nanotime(&nowtm);
 
-	if (node->tn_status & TMPFS_NODE_ACCESSED) {
-		node->tn_atime = acc ? *acc : nowtm;
-	}
-	if (node->tn_status & TMPFS_NODE_MODIFIED) {
-		node->tn_mtime = mod ? *mod : nowtm;
-	}
-	if (node->tn_status & TMPFS_NODE_CHANGED) {
+	if (tm == NULL)
+		nanotime(&nowtm);
+	else
+		nowtm = *tm;
+
+	if (node->tn_status & TMPFS_NODE_ACCESSED)
+		node->tn_atime = nowtm;
+	if (node->tn_status & TMPFS_NODE_MODIFIED)
+		node->tn_mtime = nowtm;
+	if (node->tn_status & TMPFS_NODE_CHANGED)
 		node->tn_ctime = nowtm;
-	}
 
 	node->tn_status &= ~TMPFS_NODE_STATUSALL;
 }
@@ -1218,7 +1211,7 @@ tmpfs_truncate(struct vnode *vp, off_t length)
 out:
 	if (error == 0)
 		VN_KNOTE(vp, NOTE_ATTRIB);
-	tmpfs_update(vp, NULL, NULL, 0);
+	tmpfs_update(node, NULL);
 	return error;
 }
 

sys/tmpfs/tmpfs.h
@@ -306,8 +306,7 @@ int		tmpfs_chsize(struct vnode *, u_quad_t, struct ucred *,
 int		tmpfs_chtimes(struct vnode *, const struct timespec *,
 		    const struct timespec *, int, struct ucred *,
 		    struct proc *);
-void		tmpfs_update(struct vnode *, const struct timespec *,
-		    const struct timespec *, int);
+void		tmpfs_update(tmpfs_node_t *, const struct timespec *);
 int		tmpfs_zeropg(tmpfs_node_t *, voff_t, vaddr_t);
 int		tmpfs_uio_cached(tmpfs_node_t *);
 int		tmpfs_uiomove(tmpfs_node_t *, struct uio *, vsize_t);

sys/tmpfs/tmpfs_fifoops.c
@@ -93,7 +93,7 @@ tmpfs_fifo_close(void *v)
 	} */ *ap = v;
 	struct vnode *vp = ap->a_vp;
 
-	tmpfs_update(vp, NULL, NULL, 0);
+	tmpfs_update(VP_TO_TMPFS_NODE(vp), NULL);
 	return (fifo_close(v));
 }
 

sys/tmpfs/tmpfs_specops.c
@@ -95,7 +95,7 @@ tmpfs_spec_close(void *v)
 	} */ *ap = v;
 	struct vnode *vp = ap->a_vp;
 
-	tmpfs_update(vp, NULL, NULL, 0);
+	tmpfs_update(VP_TO_TMPFS_NODE(vp), NULL);
 	return (spec_close(ap));
 }
 

sys/tmpfs/tmpfs_vnops.c
@@ -396,7 +396,8 @@ tmpfs_close(void *v)
 
 	KASSERT(VOP_ISLOCKED(vp));
 
-	tmpfs_update(vp, NULL, NULL, 0);
+	tmpfs_update(VP_TO_TMPFS_NODE(vp), NULL);
+
 	return 0;
 }
 
@@ -454,7 +455,7 @@ tmpfs_getattr(void *v)
 
 	vattr_null(vap);
 
-	tmpfs_update(vp, NULL, NULL, 0);
+	tmpfs_update(node, NULL);
 
 	vap->va_type = vp->v_type;
 	vap->va_mode = node->tn_mode;
@@ -543,7 +544,8 @@ tmpfs_setattr(void *v)
 	}
 
 bail:
-	tmpfs_update(vp, NULL, NULL, 0);
+	tmpfs_update(VP_TO_TMPFS_NODE(vp), NULL);
+
 	return error;
 }
 
@@ -693,9 +695,11 @@ tmpfs_fsync(void *v)
 	} */ *ap = v;
 	struct vnode *vp = ap->a_vp;
 
-	/* Nothing to do.  Just update. */
 	KASSERT(VOP_ISLOCKED(vp));
-	tmpfs_update(vp, NULL, NULL, 0);
+
+	/* Nothing to do.  Just update. */
+	tmpfs_update(VP_TO_TMPFS_NODE(vp), NULL);
+
 	return 0;
 }
 
@@ -765,7 +769,7 @@ tmpfs_remove(void *v)
 	if (node->tn_links > 0)  {
 		/* We removed a hard link. */
 		node->tn_status |= TMPFS_NODE_CHANGED;
-		tmpfs_update(vp, NULL, NULL, 0);
+		tmpfs_update(node, NULL);
 	}
 	error = 0;
 out:
@@ -844,7 +848,7 @@ tmpfs_link(void *v)
 		VN_KNOTE(node->tn_vnode, NOTE_LINK);
 	}
 	node->tn_status |= TMPFS_NODE_CHANGED;
-	tmpfs_update(vp, NULL, NULL, 0);
+	tmpfs_update(node, NULL);
 	error = 0;
 out:
 	pool_put(&namei_pool, cnp->cn_pnbuf);

commit 42c8dfc632f598d1a1c5e35b6134dcda04f8d879

Clear the TMPFS_NODE_{ACCESS,MODIFI}ED flags of nodes passed to tmpfs_chtimes(). Fixes a regression introduced in 747733d8.
OK natano@.

sys/tmpfs/tmpfs_subr.c
@@ -1145,10 +1145,15 @@ tmpfs_chtimes(struct vnode *vp, const struct timespec *atime,
 	    (error = VOP_ACCESS(vp, VWRITE, cred))))
 		return error;
 
-	if (atime->tv_nsec != UTIME_OMIT)
+	if (atime->tv_nsec != UTIME_OMIT) {
+		node->tn_status &= ~TMPFS_NODE_ACCESSED;
 		node->tn_atime = *atime;
-	if (mtime->tv_nsec != UTIME_OMIT)
+	}
+
+	if (mtime->tv_nsec != UTIME_OMIT) {
+		node->tn_status &= ~TMPFS_NODE_MODIFIED;
 		node->tn_mtime = *mtime;
+	}
 
 	VN_KNOTE(vp, NOTE_ATTRIB);
 