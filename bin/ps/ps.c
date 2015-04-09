6ce97ee1be4f553b0c843c84bdc12c429d6b89d4
Okan Demirmen <okan@cvs.openbsd.org>
2015-04-09 19:48:26 +0000

    kflag and wflag don't need to be counters; wflag only needs to know if greater than 0 for its one test case.
    ok deraadt

--- bin/ps/ps.c
+++ bin/ps/ps.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ps.c,v 1.62 2014/07/08 23:31:22 deraadt Exp $	*/
+/*	$OpenBSD: ps.c,v 1.64 2015/04/09 19:48:25 okan Exp $	*/
 /*	$NetBSD: ps.c,v 1.15 1995/05/18 20:33:25 mycroft Exp $	*/
 
 /*-
@@ -176,7 +176,7 @@ main(int argc, char *argv[])
 			jfmt[0] = '\0';
 			break;
 		case 'k':
-			kflag++;
+			kflag = 1;
 			break;
 		case 'L':
 			showkey();
@@ -251,7 +251,7 @@ main(int argc, char *argv[])
 				termwidth = UNLIMITED;
 			else if (termwidth < 131)
 				termwidth = 131;
-			wflag++;
+			wflag = 1;
 			break;
 		case 'x':
 			xflg = 1;
