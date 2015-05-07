677df456af8079494436f0f11f74c5fba70a1e5d
Jonathan Gray <jsg@cvs.openbsd.org>
2015-05-07 01:55:45 +0000

    fix indentation ok miod@

--- sys/uvm/uvm_aobj.c
+++ sys/uvm/uvm_aobj.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uvm_aobj.c,v 1.76 2014/12/23 04:56:47 tedu Exp $	*/
+/*	$OpenBSD: uvm_aobj.c,v 1.79 2015/05/07 01:55:44 jsg Exp $	*/
 /*	$NetBSD: uvm_aobj.c,v 1.39 2001/02/18 21:19:08 chs Exp $	*/
 
 /*
@@ -1301,7 +1301,7 @@ uao_get(struct uvm_object *uobj, voff_t offset, struct vm_page **pps,
 				    (flags & PGO_ALLPAGES) != 0)
 					/* need to do a wait or I/O! */
 					done = FALSE;	
-					continue;
+				continue;
 			}
 
 			/*
