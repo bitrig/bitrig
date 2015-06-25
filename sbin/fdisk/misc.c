pruiz/openbsd-src

Expand internal representation of partition starting LBA and size to 64 bits, adjusting printf()'s, etc. as required. Preparing for GPT partitions. Add 't' size unit. No other intentional functional change.

sbin/fdisk/misc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: misc.c,v 1.45 2015/03/16 23:51:50 krw Exp $	*/
+/*	$OpenBSD: misc.c,v 1.46 2015/03/26 14:08:12 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -191,8 +191,8 @@ ask_yn(const char *str)
 /*
  * adapted from sbin/disklabel/editor.c
  */
-u_int32_t
-getuint(char *prompt, u_int32_t oval, u_int32_t maxval)
+u_int64_t
+getuint64(char *prompt, u_int64_t oval, u_int64_t maxval)
 {
 	char buf[BUFSIZ], *endptr, *p, operator = '\0';
 	size_t n;
@@ -207,7 +207,7 @@ getuint(char *prompt, u_int32_t oval, u_int32_t maxval)
 	secpercyl = disk.sectors * disk.heads;
 
 	do {
-		printf("%s: [%u] ", prompt, oval);
+		printf("%s: [%llu] ", prompt, oval);
 
 		if (fgets(buf, sizeof(buf), stdin) == NULL)
 			errx(1, "eof");
@@ -231,7 +231,7 @@ getuint(char *prompt, u_int32_t oval, u_int32_t maxval)
 			break;
 		case 'b':
 			unit = 'b';
-			mult = -secsize;
+			mult = -(int64_t)secsize;
 			buf[--n] = '\0';
 			break;
 		case 's':
@@ -242,19 +242,24 @@ getuint(char *prompt, u_int32_t oval, u_int32_t maxval)
 		case 'k':
 			unit = 'k';
 			if (secsize > 1024)
-				mult = -secsize / 1024;
+				mult = -(int64_t)secsize / 1024LL;
 			else
-				mult = 1024 / secsize;
+				mult = 1024LL / secsize;
 			buf[--n] = '\0';
 			break;
 		case 'm':
 			unit = 'm';
-			mult = 1048576 / secsize;
+			mult = (1024LL * 1024) / secsize;
 			buf[--n] = '\0';
 			break;
 		case 'g':
 			unit = 'g';
-			mult = 1073741824 / secsize;
+			mult = (1024LL * 1024 * 1024) / secsize;
+			buf[--n] = '\0';
+			break;
+		case 't':
+			unit = 't';
+			mult = (1024LL * 1024 * 1024 * 1024) / secsize;
 			buf[--n] = '\0';
 			break;
 		default:
@@ -301,5 +306,5 @@ getuint(char *prompt, u_int32_t oval, u_int32_t maxval)
 		}
 	} while (1);
 
-	return ((u_int32_t)d);
+	return((u_int64_t)d);
 }

sbin/fdisk/part.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: part.c,v 1.69 2015/03/16 23:51:50 krw Exp $	*/
+/*	$OpenBSD: part.c,v 1.70 2015/03/26 14:08:12 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -222,7 +222,7 @@ PRT_make(struct prt *partn, off_t offset, off_t reloff,
 {
 	off_t off;
 	u_int32_t ecsave, scsave;
-	u_int32_t t;
+	u_int64_t t;
 
 	/* Save (and restore below) cylinder info we may fiddle with. */
 	scsave = partn->scyl;
@@ -255,15 +255,10 @@ PRT_make(struct prt *partn, off_t offset, off_t reloff,
 	prt->dp_flag = partn->flag & 0xFF;
 	prt->dp_typ = partn->id & 0xFF;
 
-#if 0 /* XXX */
-	prt->dp_start = htole32(partn->bs - off);
-	prt->dp_size = htole32(partn->ns);
-#else
-	t = htole32(partn->bs - off);
+	t = htole64(partn->bs - off);
 	memcpy(&prt->dp_start, &t, sizeof(u_int32_t));
-	t = htole32(partn->ns);
+	t = htole64(partn->ns);
 	memcpy(&prt->dp_size, &t, sizeof(u_int32_t));
-#endif
 
 	partn->scyl = scsave;
 	partn->ecyl = ecsave;
@@ -284,7 +279,7 @@ PRT_print(int num, struct prt *partn, char *units)
 	} else {
 		size = ((double)partn->ns * unit_types[SECTORS].conversion) /
 		    unit_types[i].conversion;
-		printf("%c%1d: %.2X %6u %3u %3u - %6u %3u %3u [%12u:%12.0f%s] %s\n",
+		printf("%c%1d: %.2X %6u %3u %3u - %6u %3u %3u [%12llu:%12.0f%s] %s\n",
 		    (partn->flag == 0x80)?'*':' ',
 		    num, partn->id,
 		    partn->scyl, partn->shead, partn->ssect,

sbin/fdisk/part.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: part.h,v 1.18 2015/03/16 23:51:50 krw Exp $	*/
+/*	$OpenBSD: part.h,v 1.19 2015/03/26 14:08:12 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -20,10 +20,10 @@
 #define _PART_H
 
 struct prt {
+	u_int64_t bs;
+	u_int64_t ns;
 	u_int32_t shead, scyl, ssect;
 	u_int32_t ehead, ecyl, esect;
-	u_int32_t bs;
-	u_int32_t ns;
 	unsigned char flag;
 	unsigned char id;
 };

bitrig/bitrig

sbin/fdisk/misc.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: misc.c,v 1.45 2015/03/16 23:51:50 krw Exp $	*/
+/*	$OpenBSD: misc.c,v 1.46 2015/03/26 14:08:12 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -187,8 +187,8 @@ ask_yn(const char *str)
 /*
  * adapted from sbin/disklabel/editor.c
  */
-uint32_t
-getuint(char *prompt, uint32_t oval, uint32_t maxval)
+uint64_t
+getuint64(char *prompt, uint64_t oval, uint64_t maxval)
 {
 	char buf[BUFSIZ], *endptr, *p, operator = '\0';
 	size_t n;
@@ -203,7 +203,7 @@ getuint(char *prompt, uint32_t oval, uint32_t maxval)
 	secpercyl = disk.sectors * disk.heads;
 
 	do {
-		printf("%s: [%u] ", prompt, oval);
+		printf("%s: [%llu] ", prompt, oval);
 
 		if (fgets(buf, sizeof(buf), stdin) == NULL)
 			errx(1, "eof");
@@ -227,7 +227,7 @@ getuint(char *prompt, uint32_t oval, uint32_t maxval)
 			break;
 		case 'b':
 			unit = 'b';
-			mult = -secsize;
+			mult = -(int64_t)secsize;
 			buf[--n] = '\0';
 			break;
 		case 's':
@@ -238,19 +238,24 @@ getuint(char *prompt, uint32_t oval, uint32_t maxval)
 		case 'k':
 			unit = 'k';
 			if (secsize > 1024)
-				mult = -secsize / 1024;
+				mult = -(int64_t)secsize / 1024LL;
 			else
-				mult = 1024 / secsize;
+				mult = 1024LL / secsize;
 			buf[--n] = '\0';
 			break;
 		case 'm':
 			unit = 'm';
-			mult = 1048576 / secsize;
+			mult = (1024LL * 1024) / secsize;
 			buf[--n] = '\0';
 			break;
 		case 'g':
 			unit = 'g';
-			mult = 1073741824 / secsize;
+			mult = (1024LL * 1024 * 1024) / secsize;
+			buf[--n] = '\0';
+			break;
+		case 't':
+			unit = 't';
+			mult = (1024LL * 1024 * 1024 * 1024) / secsize;
 			buf[--n] = '\0';
 			break;
 		default:
@@ -297,7 +302,7 @@ getuint(char *prompt, uint32_t oval, uint32_t maxval)
 		}
 	} while (1);
 
-	return ((uint32_t)d);
+	return ((uint64_t)d);
 }
 
 void

sbin/fdisk/part.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: part.c,v 1.69 2015/03/16 23:51:50 krw Exp $	*/
+/*	$OpenBSD: part.c,v 1.70 2015/03/26 14:08:12 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -224,7 +224,7 @@ PRT_make(struct prt *partn, off_t offset, off_t reloff,
 {
 	off_t off;
 	uint32_t ecsave, scsave;
-	uint32_t t;
+	uint64_t t;
 
 	/* Save (and restore below) cylinder info we may fiddle with. */
 	scsave = partn->scyl;
@@ -257,15 +257,10 @@ PRT_make(struct prt *partn, off_t offset, off_t reloff,
 	prt->dp_flag = partn->flag & 0xFF;
 	prt->dp_typ = partn->id & 0xFF;
 
-#if 0 /* XXX */
-	prt->dp_start = htole32(partn->bs - off);
-	prt->dp_size = htole32(partn->ns);
-#else
-	t = htole32(partn->bs - off);
+	t = htole64(partn->bs - off);
 	memcpy(&prt->dp_start, &t, sizeof(uint32_t));
-	t = htole32(partn->ns);
+	t = htole64(partn->ns);
 	memcpy(&prt->dp_size, &t, sizeof(uint32_t));
-#endif
 
 	partn->scyl = scsave;
 	partn->ecyl = ecsave;
@@ -286,7 +281,7 @@ PRT_print(int num, struct prt *partn, char *units)
 	} else {
 		size = ((double)partn->ns * unit_types[SECTORS].conversion) /
 		    unit_types[i].conversion;
-		printf("%c%1d: %.2X %6u %3u %3u - %6u %3u %3u [%12u:%12.0f%s] %s\n",
+		printf("%c%1d: %.2X %6u %3u %3u - %6u %3u %3u [%12llu:%12.0f%s] %s\n",
 		    (partn->flag == DOSACTIVE)?'*':' ',
 		    num, partn->id,
 		    partn->scyl, partn->shead, partn->ssect,

sbin/fdisk/part.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: part.h,v 1.18 2015/03/16 23:51:50 krw Exp $	*/
+/*	$OpenBSD: part.h,v 1.19 2015/03/26 14:08:12 krw Exp $	*/
 
 /*
  * Copyright (c) 1997 Tobias Weingartner
@@ -20,10 +20,10 @@
 #define _PART_H
 
 struct prt {
+	uint64_t bs;
+	uint64_t ns;
 	uint32_t shead, scyl, ssect;
 	uint32_t ehead, ecyl, esect;
-	uint32_t bs;
-	uint32_t ns;
 	unsigned char flag;
 	unsigned char id;
 };