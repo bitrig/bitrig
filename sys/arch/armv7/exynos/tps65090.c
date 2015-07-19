82c67a5ab688f471f94f1841182ce07ffad7e936
Brandon Mercer <bmercer@cvs.openbsd.org>
2015-07-19 02:41:40 +0000

    Rename the tpspmic driver so it does not contain numbers. OK jsg@

--- sys/arch/armv7/exynos/tps65090.c
+++ sys/arch/armv7/exynos/tps65090.c
@@ -1,4 +1,4 @@
-/* $OpenBSD$ */
+/* $OpenBSD: tps65090.c,v 1.2 2015/07/19 02:41:39 bmercer Exp $ */
 /*
  * Copyright (c) 2013 Patrick Wildt <patrick@blueri.se>
  *
@@ -89,7 +89,7 @@ struct cfattach tpspmic_ca = {
 };
 
 struct cfdriver tpspmic_cd = {
-	NULL, "tps65090", DV_DULL
+	NULL, "tpspmic", DV_DULL
 };
 
 int
