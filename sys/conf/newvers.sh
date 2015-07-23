04372076d6686ddde086a8a47c1b62cdc4af04b9
Theo de Raadt <deraadt@cvs.openbsd.org>
2015-07-23 16:26:58 +0000

    remove -beta tag.  take that as a hint.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.137 2015/03/09 20:08:55 miod Exp $
+#	$OpenBSD: newvers.sh,v 1.139 2015/07/23 16:26:57 deraadt Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
