ce57bc2422d8b271e1ecd0f3d8e83a87d198f003
Jeremie Courreges-Anglas <jca@cvs.openbsd.org>
2015-08-10 20:31:01 +0000

    Back to -current.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.139 2015/07/23 16:26:57 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.140 2015/08/10 20:31:00 jca Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
