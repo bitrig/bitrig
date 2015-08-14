bb10b9ba730a16a08d787d6ecc4b82ea57751d67
Kenneth R Westerback <krw@cvs.openbsd.org>
2015-08-14 23:45:57 +0000

    The disktab attribute 'ty' was used to set D_REMOVEABLE and D_RAMDISK flags in the disklabel. These flags were discarded in 2006.
    Remove documentation for and instances of 'ty'. Add instances of
    'dt' that were missing. Use 'floppy' as the value of 'dt' for all
    floppies and 'rdroot' for all boot images/miniroots.
    
    Add 'rdroot' to dktypenames and an associated #define DTYPE_RDROOT,
    since 'dt' values are parsed by matching entries in dktypenames[].
    
    Slightly tweaked part of larger cleanup diff previously ok deraadt@.

--- sys/sys/disklabel.h
+++ sys/sys/disklabel.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: disklabel.h,v 1.66 2015/07/23 18:02:59 krw Exp $	*/
+/*	$OpenBSD: disklabel.h,v 1.67 2015/08/14 23:45:56 krw Exp $	*/
 /*	$NetBSD: disklabel.h,v 1.41 1996/05/10 23:07:37 mark Exp $	*/
 
 /*
