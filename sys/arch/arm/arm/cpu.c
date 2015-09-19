7334b8a627d0e2ecea662faff78360dc9f92f510
Jonathan Gray <jsg@cvs.openbsd.org>
2015-09-19 02:13:06 +0000

    intr_barrier(9) for arm ok kettenis@

--- sys/arch/arm/arm/cpu.c
+++ sys/arch/arm/arm/cpu.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.c,v 1.21 2015/05/29 05:48:07 jsg Exp $	*/
+/*	$OpenBSD: cpu.c,v 1.22 2015/09/19 02:13:05 jsg Exp $	*/
 /*	$NetBSD: cpu.c,v 1.56 2004/04/14 04:01:49 bsh Exp $	*/
 
 
@@ -51,8 +51,10 @@
 #include <sys/user.h>
 #include <sys/proc.h>
 #include <sys/conf.h>
+#include <sys/sched.h>
 #include <uvm/uvm_extern.h>
 #include <machine/cpu.h>
+#include <machine/intr.h>
 
 #include <arm/cpuconf.h>
 #include <arm/undefined.h>
@@ -423,5 +425,11 @@ cpu_alloc_arm_stack(struct cpu_info *ci)
 	return 0;
 }
 #endif /* MULTIPROCESSOR */
+
+void
+intr_barrier(void *ih)
+{
+	sched_barrier(NULL);
+}
 
 /* End of cpu.c */
