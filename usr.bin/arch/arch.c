ajinkya93/OpenBSD

No need for err.h, avoid excess fputs() calls and return from main instead of calling exit().  From Michael Reed.

usr.bin/arch/arch.c
@@ -25,7 +25,6 @@
 
 #include <sys/param.h>	/* MACHINE MACHINE_ARCH */
 
-#include <err.h>
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -73,13 +72,10 @@ main(int argc, char *argv[])
 		usage();
 		/* NOTREACHED */
 	}
-	if (!short_form) {
-		fputs("OpenBSD", stdout);
-		fputc('.', stdout);
-	}
-	fputs(arch, stdout);
-	fputc('\n', stdout);
-	exit(0);
+	if (!short_form)
+		fputs("OpenBSD.", stdout);
+	puts(arch);
+	return 0;
 }
 
 static void

bitrig/bitrig

usr.bin/arch/arch.c
@@ -25,7 +25,6 @@
 
 #include <sys/param.h>	/* MACHINE MACHINE_ARCH */
 
-#include <err.h>
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -73,13 +72,10 @@ main(int argc, char *argv[])
 		usage();
 		/* NOTREACHED */
 	}
-	if (!short_form) {
-		fputs("Bitrig", stdout);
-		fputc('.', stdout);
-	}
-	fputs(arch, stdout);
-	fputc('\n', stdout);
-	exit(0);
+	if (!short_form)
+		fputs("Bitrig.", stdout);
+	puts(arch);
+	return 0;
 }
 
 static void