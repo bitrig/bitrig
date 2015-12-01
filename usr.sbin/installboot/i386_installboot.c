8a0dddd7c06b740a6e36a5c225e985225812dda1
krw <krw@openbsd.org>
2015-12-01 06:39:52 +0000

    Don't need <sys/types.h> and <sys/param.h>. Nuke the former when DEV_BSIZE is needed.

--- usr.sbin/installboot/i386_installboot.c
+++ usr.sbin/installboot/i386_installboot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: i386_installboot.c,v 1.17 2015/11/26 19:03:10 deraadt Exp $	*/
+/*	$OpenBSD: i386_installboot.c,v 1.19 2015/12/01 06:39:52 krw Exp $	*/
 /*	$NetBSD: installboot.c,v 1.5 1995/11/17 23:23:50 gwr Exp $ */
 
 /*
@@ -39,7 +39,6 @@
 #define ELFSIZE 32
 
 #include <sys/param.h>	/* DEV_BSIZE */
-#include <sys/types.h>
 #include <sys/disklabel.h>
 #include <sys/dkio.h>
 #include <sys/ioctl.h>
