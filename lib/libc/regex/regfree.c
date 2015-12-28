10474f4a9dafa8bcc477e1944070a0cb81c93a48
mmcc <mmcc@openbsd.org>
2015-12-28 22:27:03 +0000

    Remove NULL-checks before free() and unnecessary argument casts.
    ok tb@

--- lib/libc/regex/regfree.c
+++ lib/libc/regex/regfree.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: regfree.c,v 1.10 2015/12/28 22:08:18 mmcc Exp $ */
+/*	$OpenBSD: regfree.c,v 1.11 2015/12/28 22:27:03 mmcc Exp $ */
 /*-
  * Copyright (c) 1992, 1993, 1994 Henry Spencer.
  * Copyright (c) 1992, 1993, 1994
@@ -63,18 +63,10 @@ regfree(regex_t *preg)
 	preg->re_magic = 0;		/* mark it invalid */
 	g->magic = 0;			/* mark it invalid */
 
-	if (g->strip != NULL)
-		free((char *)g->strip);
-	if (g->sets != NULL) {
-		for (i = 0; i < g->ncsets; i++) {
-			free(g->sets[i].ranges);
-			free(g->sets[i].wides);
-			free(g->sets[i].types);
-		}
-		free((char *)g->sets);
-	if (g->setbits != NULL)
-		free((char *)g->setbits);
+	free(g->strip);
+	free(g->sets);
+	free(g->setbits);
 	free(g->must);
-	free((char *)g);
+	free(g);
 }
 DEF_WEAK(regfree);
