668dbb9beccde02f5c747785f6b09d1ab04e0f13
Marco Peereboom <marco@peereboom.us>
2016-03-04 13:45:06 -0600

    allow SMALL_KERNEL so that merges are simpler

--- sys/net/radix.c
+++ sys/net/radix.c
@@ -56,6 +56,7 @@
 #include <sys/socket.h>
 #include <net/route.h>
 #include <net/radix_mpath.h>
+#endif
 
 static unsigned int max_keylen;
 struct radix_node_head *mask_rnhead;
@@ -631,7 +632,9 @@ rn_add_dupedkey(struct radix_node *saved_tt, struct radix_node_head *head,
 {
 	caddr_t netmask = tt->rn_mask;
 	struct radix_node *x = saved_tt, *xp;
+#ifndef SMALL_KERNEL
 	struct radix_node *dupedkey_tt = NULL;
+#endif
 	int before = -1;
 	int b_leaf = 0;
 
@@ -639,6 +642,7 @@ rn_add_dupedkey(struct radix_node *saved_tt, struct radix_node_head *head,
 		b_leaf = tt->rn_b;
 
 	for (xp = x; x; xp = x, x = x->rn_dupedkey) {
+#ifndef SMALL_KERNEL
 		/* permit multipath, if enabled for the family */
 		if (rn_mpath_capable(head) && netmask == x->rn_mask) {
 			int mid;
@@ -677,6 +681,7 @@ rn_add_dupedkey(struct radix_node *saved_tt, struct radix_node_head *head,
 			} while (x && --mid > 0);
 			break;
 		}
+#endif
 		if (x->rn_mask == netmask)
 			return (-1);
 		if (netmask == NULL ||
@@ -707,11 +712,13 @@ rn_add_dupedkey(struct radix_node *saved_tt, struct radix_node_head *head,
 	rn_link_dupedkey(tt, xp, before);
 
 
+#ifndef SMALL_KERNEL
 	/* adjust the flags of the possible multipath chain */
 	if (!dupedkey_tt)
 		dupedkey_tt = tt;
 	if (rn_mpath_capable(head))
 		rn_mpath_adj_mpflag(dupedkey_tt, prio);
+#endif
 	return (0);
 }
 
@@ -846,8 +853,10 @@ rn_addroute(void *v_arg, void *n_arg, struct radix_node_head *head,
 			return (NULL);
 	} else {
 		rn_fixup_nodes(tt);
+#ifndef SMALL_KERNEL
 		if (rn_mpath_capable(head))
 			rn_mpath_adj_mpflag(tt, prio);
+#endif
 	}
 
 	/* finally insert a radix_mask element if needed */
@@ -998,9 +1007,11 @@ rn_delete(void *v_arg, void *n_arg, struct radix_node_head *head,
 	/* save start of multi path chain for later use */
 	dupedkey_tt = tt;
 
+#ifndef SMALL_KERNEL
 	/* if we got a hint use the hint from now on */
 	if (rn)
 		tt = rn;
+#endif
 
 	KASSERT((tt->rn_flags & RNF_ROOT) == 0);
 
@@ -1038,10 +1049,12 @@ rn_delete(void *v_arg, void *n_arg, struct radix_node_head *head,
 		if  (tt[1].rn_flags & RNF_ACTIVE)
 			rn_swap_nodes(&tt[1], &x[1]);
 
+#ifndef SMALL_KERNEL
 		/* adjust the flags of the multipath chain */
 		if (rn_mpath_capable(head))
 			rn_mpath_adj_mpflag(dupedkey_tt,
 			    ((struct rtentry *)tt)->rt_priority);
+#endif
 		/* over and out */
 		goto out;
 	}
